36816d7ba6917d95f9a4ef4c00b9e1b4e0e56585
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2013-02-15 15:29:42 +0100

    Remove hard-coded configuration paths

--- autofocus.py
+++ autofocus.py
@@ -1,13 +1,13 @@
+import logging
 import controller.crio
 from process.gradientmaximizer import GradientMaximizer
 from measure.maximization import Maximizer
-import logging
 
 from gi.repository import Ufo, Uca
 
 
 if __name__ == '__main__':
-    config = Ufo.Config(paths=['/home/farago/dev/ufo/ufo-filters/build/src'])
+    config = Ufo.Config()
 
     logger = logging.getLogger()
     logger.addHandler(logging.StreamHandler())
