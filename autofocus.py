58d18d37480623087063c833f953632dbe6518dc
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2013-02-18 09:06:10 +0100

    Make camera type a command line argument

--- autofocus.py
+++ autofocus.py
@@ -1,4 +1,5 @@
 import logging
+import argparse
 import controller.crio
 from process.gradientmaximizer import GradientMaximizer
 from measure.optimization import Maximizer
@@ -9,12 +10,18 @@ from gi.repository import Ufo, Uca
 if __name__ == '__main__':
     config = Ufo.Config()
 
+    parser = argparse.ArgumentParser(description='Run autofocus procedure')
+    parser.add_argument('--camera', type=str, default='mock',
+                        help='libuca camera identifier')
+
+    args = parser.parse_args()
+
     logger = logging.getLogger()
     logger.addHandler(logging.StreamHandler())
     logger.setLevel(logging.INFO)
 
     pm = Uca.PluginManager()
-    camera = pm.get_camera('mock')
+    camera = pm.get_camera(args.camera)
 
     sharpness = Maximizer()
     lm = controller.crio.LinearMotor()
