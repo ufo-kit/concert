7673674398216af7be52c53773770ee95d5e66e8
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2013-03-08 20:42:57 +0100

    Add calibration test and docstrings

--- control/devices/motion/axes/calibration.py
+++ control/devices/motion/axes/calibration.py
@@ -7,12 +7,20 @@ class _Calibration(object):
 
 
 class LinearCalibration(_Calibration):
-    def __init__(self, steps_per_unit, offset):
+    """Represents a linear calibration.
+
+    A linear calibration maps a number of motor steps to a real-world unit
+    system.
+
+    """
+    def __init__(self, steps_per_unit, offset_in_steps):
         self._steps_per_unit = steps_per_unit
-        self._offset = offset
+        self._offset = offset_in_steps
 
     def to_user(self, value_in_steps):
+        """Convert value_in_steps to user units"""
         return value_in_steps / self._steps_per_unit + self._offset
 
     def to_steps(self, value):
+        """Convert user unit value to motor steps"""
         return (value - self._offset) * self._steps_per_unit
---
+++ control/tests/test_calibration.py
@@ -0,0 +1,20 @@
+import unittest
+import quantities as q
+from control.devices.motion.axes.calibration import LinearCalibration
+
+
+class TestLinearCalibration(unittest.TestCase):
+    STEPS_PER_UNIT = 5000 * q.dimensionless
+    OFFSET = 0 * q.mm 
+
+    def setUp(self):
+        self.calibration = LinearCalibration(self.STEPS_PER_UNIT / q.mm,
+                                             self.OFFSET)
+
+    def test_to_user(self):
+        user = self.calibration.to_user(self.STEPS_PER_UNIT)
+        self.assertEqual(user, 1 * q.mm + self.OFFSET)
+
+    def test_to_steps(self):
+        steps = self.calibration.to_steps(1 * q.mm)
+        self.assertEqual(steps, self.STEPS_PER_UNIT)
