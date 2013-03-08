b13c3239d3ba6ccbfa0e58841a2498832d1c8a8b
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2013-03-08 10:08:03 +0100

    Refactor ANKA storage ring

--- src/control/devices/storagerings/ankatangostoragering.py
+++ src/control/devices/storagerings/ankatangostoragering.py
@@ -11,16 +11,18 @@ class ANKATangoStorageRing(StorageRing):
     def __init__(self, connection):
         super(ANKATangoStorageRing, self).__init__()
         self._connection = connection
-        
+
+    def _get_tango_value(self, attrib):
+        return self._connection.tango_device.read_attribute(attrib).value
+
     @property
     def current(self):
-        return self._connection.tango_device.read_attribute("ECurrent").value*\
-                                                                        pq.mA
+        return self._get_tango_value("ECurrent").value * pq.mA
+
     @property
     def energy(self):
-        return self._connection.tango_device.read_attribute("Energy").value*\
-                                                                        pq.MeV
+        return self._get_tango_value("Energy").value * pq.MeV
+
     @property
     def lifetime(self):
-        return self._connection.tango_device.read_attribute("Lifetime").value*\
-                                                                        pq.hour
+        return self._get_tango_value("Lifetime").value * pq.hour
