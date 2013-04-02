64b868115e68b904d4940a8500326cff44cd1da4
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2013-04-02 15:18:03 +0200

    We don't need setter for owner-based notification

--- concert/devices/storagerings/base.py
+++ concert/devices/storagerings/base.py
@@ -9,24 +9,18 @@ from concert.base import Device, Parameter
 
 class StorageRing(Device):
     def __init__(self):
-        def do_nothing(value):
-            pass
-
         params = [Parameter('current',
                             fget=self._get_current,
-                            fset=do_nothing,
                             unit=q.mA,
                             doc="Current of the ring",
                             owner_only=True),
                   Parameter('energy',
                             fget=self._get_energy,
-                            fset=do_nothing,
                             unit=q.MeV,
                             doc="Energy of the ring",
                             owner_only=True),
                   Parameter('lifetime',
                             fget=self._get_lifetime,
-                            fset=do_nothing,
                             unit=q.h,
                             doc="Lifetime of the ring",
                             owner_only=True)]
