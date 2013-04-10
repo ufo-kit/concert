5669a201376de37a05dd3239686a1e94a75cb888
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2013-04-10 17:29:40 +0200

    Split feedback from scanner process
    ... and so the circle closes.

--- concert/processes/base.py
+++ concert/processes/base.py
@@ -1,5 +1,6 @@
 import numpy as np
 from concert.base import Parameterizable, Parameter
+from concert.asynchronous import async
 
 
 class Process(Parameterizable):
@@ -7,11 +8,20 @@ class Process(Parameterizable):
     def __init__(self, params):
         super(Process, self).__init__(params)
 
+    @async
     def run(self):
         """Run the process.
 
         The result depends on the actual process.
         """
+        raise NotImplementedError
+
+
+class Feedback(object):
+    """Provide a feedback."""
+
+    def evaluate(self):
+        """This must return a value. Any kind of value."""
         raise NotImplementedError
 
 
@@ -22,21 +32,23 @@ class Scanner(Process):
     call :meth:`.evaluate`.
     """
 
-    def __init__(self, param):
+    def __init__(self, param, feedback):
         params = [Parameter('minimum', doc="Left bound of the interval"),
                   Parameter('maximum', doc="Right bound of the interval"),
                   Parameter('intervals', doc="Number of intervals")]
 
         super(Scanner, self).__init__(params)
         self._param = param
+        self._feedback = feedback
 
+    @async
     def run(self):
         xs = np.linspace(self.minimum, self.maximum, self.intervals)
         ys = np.zeros(xs.shape)
 
         for i, x in enumerate(xs):
             self._param.set(x).wait()
-            ys[i] = self.evaluate()
+            ys[i] = self._feedback.evaluate()
 
         return (xs, ys)
 
--- concert/processes/camera.py
+++ concert/processes/camera.py
@@ -1,24 +1,34 @@
-import quantities as q
 import numpy as np
-from concert.processes.base import Scanner
+from concert.processes.base import Feedback, Scanner
 
 
-class NoiseScan(Scanner):
+class FrameMean(Feedback):
     """Calculate the noise level depending on a variable parameter.
 
     *camera* is a camera object that supports :meth:`.grab`, *param_name*
     is a parameter name of *camera* that should be varied according.
     """
-    def __init__(self, camera, param_name):
-        super(NoiseScan, self).__init__(camera[param_name])
-
-        self._camera = camera
-        self.minimum = 0.01 * q.s
-        self.maximum = 1.0 * q.s
-        self.intervals = 50
+    def __init__(self, camera):
+        self.camera = camera
 
     def evaluate(self):
-        frame = self._camera.grab()
+        frame = self.camera.grab()
 
         # Should this be over whole frame or just those pixels above 0?
         return np.mean(frame)
+
+
+class PhotonTransfer(Feedback):
+    """Calculate the photon transfer according to procedure described by M.
+    Caselle and F. Beckmann.
+
+    *camera* is a camera object that supports the :meth:`.grab`, *dark_frame*
+    is an array-like with the same dimensions as the camera frames.
+    """
+    def __init__(self, camera, dark_frame):
+        self.camera = camera
+        self.dark_frame = dark_frame
+
+    def evaluate(self):
+        frame = self.camera.grab()
+        return np.log(np.sum(np.abs(frame - self.dark_frame)))
