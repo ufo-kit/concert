7207dd2426aae37bcb74387468e1a8606ec04468
Tomas Farago <tomas.farago@kit.edu>
2013-04-12 16:45:44 +0200

    Monochromator

--- concert/devices/monochromators/base.py
+++ concert/devices/monochromators/base.py
@@ -7,24 +7,41 @@ from concert.base import Device, Parameter
 import quantities as q
 
 
+def energy_to_wavelength(energy):
+    """Convert *energy* [eV-like] to wavelength [m]."""
+    res = q.constants.quantum.h*q.velocity.c/energy
+
+    return res.rescale(q.m)
+
+
+def wavelength_to_energy(wavelength):
+    """Convert wavelength [m-like] to energy [eV]."""
+    res = q.constants.quantum.h*q.velocity.c/wavelength
+
+    return res.rescale(q.eV)
+
+
 class Monochromator(Device):
     """Monochromator device which is used to filter the beam in order to
     get a very narrow energy bandwidth.
     """
-    def __init__(self, calibration, limiter=None):
+    def __init__(self, calibration):
         params = [Parameter("energy", self._get_energy, self._set_energy,
-                            q.eV, limiter, "Monochromatic energy")]
+                            q.eV, doc="Monochromatic energy"),
+                  Parameter("wavelength", self._get_wavelength,
+                            self._set_wavelength,
+                            q.m, doc="Monochromatic wavelength")]
         super(Monochromator, self).__init__(params)
         self._calibration = calibration
-
-    def _get_calibrated_energy(self):
-        return self._calibration.to_user(self._get_energy())
-
-    def _set_calibrated_energy(self, energy):
-        self._set_energy(self._calibration.to_steps(energy))
 
     def _get_energy(self):
         raise NotImplementedError
 
-    def _set_energy(self, steps):
+    def _set_energy(self, energy):
         raise NotImplementedError
+
+    def _get_wavelength(self):
+        return energy_to_wavelength(self._get_energy())
+
+    def _set_wavelength(self, wavelength):
+        self._set_energy(wavelength_to_energy(wavelength))
--- concert/tests/test_monochromator.py
+++ concert/tests/test_monochromator.py
@@ -7,9 +7,25 @@ import unittest
 from concert.devices.motors.base import LinearCalibration
 import quantities as q
 from concert.devices.monochromators.dummy import DummyMonochromator
+from concert.devices.monochromators import base
 
 
 class TestDummyMonochromator(unittest.TestCase):
     def setUp(self):
         calibration = LinearCalibration(1*q.eV, 0*q.eV)
         self.mono = DummyMonochromator(calibration)
+
+    def test_energy(self):
+        energy = 25*q.keV
+        self.mono.energy = energy
+        self.assertAlmostEqual(self.mono.energy, energy)
+        self.assertAlmostEqual(self.mono.wavelength,
+                               base.energy_to_wavelength(self.mono.energy))
+
+    def test_wavelength(self):
+        lam = 0.1*q.nm
+
+        self.mono.wavelength = lam
+        self.assertAlmostEqual(self.mono.wavelength, lam)
+        self.assertAlmostEqual(base.wavelength_to_energy(lam),
+                               self.mono.energy)
