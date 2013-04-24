b9644b8348433bde6c280b0ce99f6a3c4da1509b
Tomas Farago <tomas.farago@kit.edu>
2013-04-24 08:27:24 +0200

    TopoTomo table IO device added.

---
+++ concert/devices/io/topotomo.py
@@ -0,0 +1,18 @@
+from concert.devices.io.base import IO, Port
+from concert.connections.tango import TopoTomo
+
+
+class TomoTable(IO):
+    def __init__(self):
+        ports = [Port((6, 1), "input", self.read_port, None,
+                      "Trigger input to the table."),
+                 Port(6, "output", None, self.write_port,
+                      "Trigger output from the table.")]
+        self._device = TopoTomo().get_device("iss/toto/modbusTomotable")
+        super(TomoTable, self).__init__(ports)
+
+    def read_port(self, port):
+        return self._device.ReadBits(port)
+
+    def write_port(self, port, value):
+        self._device.WriteBits((port, int(bool(value))))
