125a15c34e77c4af5f457b3b6ffb0e286e0f47ed
Tomas Farago <tomas.farago@kit.edu>
2013-05-01 11:54:23 +0200

    Monochromator implementation can choose whether to implement energy or wavelength.

--- concert/devices/monochromators/base.py
+++ concert/devices/monochromators/base.py
@@ -1,7 +1,7 @@
 '''
-Created on Apr 11, 2013
-
-@author: farago
+Monochromator module. The device implementation needs to provide getters and
+setters for wither wavelength or energy, it does not matter which one. The
+conversion is handled in the base class.
 '''
 import quantities as q
 import quantities.constants.quantum as cq
@@ -45,13 +45,35 @@ class Monochromator(Device):
         self._calibration = calibration
 
     def _get_energy(self):
-        raise NotImplementedError
+        # Check which method the subclass implements, use it and handle
+        # conversions if necessary.
+        if self.__class__._get_energy != Monochromator._get_energy:
+            return self._get_energy()
+        elif self.__class__._get_wavelength != Monochromator._get_energy:
+            return wavelength_to_energy(self._get_wavelength())
+        else:
+            raise NotImplementedError
 
     def _set_energy(self, energy):
-        raise NotImplementedError
+        if self.__class__._set_energy != Monochromator._set_energy:
+            self._set_energy(energy)
+        elif self.__class__._set_wavelength != Monochromator._set_wavelength:
+            self._set_wavelength(energy_to_wavelength(energy))
+        else:
+            raise NotImplementedError
 
     def _get_wavelength(self):
-        return energy_to_wavelength(self._get_energy())
+        if self.__class__._get_energy != Monochromator._get_energy:
+            return energy_to_wavelength(self._get_energy())
+        elif self.__class__._get_wavelength != Monochromator._get_energy:
+            return self._get_wavelength()
+        else:
+            raise NotImplementedError
 
     def _set_wavelength(self, wavelength):
-        self._set_energy(wavelength_to_energy(wavelength))
+        if self.__class__._set_energy != Monochromator._set_energy:
+            self._set_energy(wavelength_to_energy(wavelength))
+        elif self.__class__._set_wavelength != Monochromator._set_wavelength:
+            self._set_wavelength(wavelength)
+        else:
+            raise NotImplementedError
--- concert/tests/test_monochromator.py
+++ concert/tests/test_monochromator.py
@@ -5,17 +5,41 @@ Created on Apr 11, 2013
 '''
 import unittest
 import logbook
+from testfixtures import ShouldRaise
 from concert.devices.motors.base import LinearCalibration
 import quantities as q
 from concert.devices.monochromators.dummy import\
     Monochromator as DummyMonochromator
 from concert.devices.monochromators import base
+from concert.devices.monochromators.base import Monochromator
+
+
+class WavelengthMonochromator(Monochromator):
+    """A monochromator which implements wavelength getter and setter. The
+    conversion needs to be handled in the base class."""
+    def __init__(self):
+        super(WavelengthMonochromator, self).__init__(self)
+        self._wavelength = None
+
+    def _get_wavelength(self):
+        return self._wavelength
+
+    def _set_wavelength(self, wavelength):
+        self._wavelength = wavelength
+
+
+class UselessMonochromator(Monochromator):
+    """A monochromator wihch does not implement anything."""
+    def __init__(self):
+        super(UselessMonochromator, self).__init__(self)
 
 
 class TestDummyMonochromator(unittest.TestCase):
     def setUp(self):
         calibration = LinearCalibration(1*q.eV, 0*q.eV)
         self.mono = DummyMonochromator(calibration)
+        self.wave_mono = WavelengthMonochromator()
+        self.useless_mono = UselessMonochromator()
         self.handler = logbook.TestHandler()
         self.handler.push_application()
 
@@ -24,15 +48,38 @@ class TestDummyMonochromator(unittest.TestCase):
 
     def test_energy(self):
         energy = 25*q.keV
+
+        # Dummy monochromator.
         self.mono.energy = energy
         self.assertAlmostEqual(self.mono.energy, energy)
         self.assertAlmostEqual(self.mono.wavelength,
                                base.energy_to_wavelength(self.mono.energy))
 
+        # Wavelength-based monochromator
+        self.wave_mono.energy = energy
+        self.assertAlmostEqual(self.wave_mono.energy, energy)
+        self.assertAlmostEqual(self.wave_mono.wavelength, base.
+                               energy_to_wavelength(self.wave_mono.energy))
+
+        # Useless monochromator.
+        with ShouldRaise(NotImplementedError):
+            self.useless_mono.energy = energy
+
     def test_wavelength(self):
         lam = 0.1*q.nm
 
+        # Dummy monochromator.
         self.mono.wavelength = lam
         self.assertAlmostEqual(self.mono.wavelength, lam)
         self.assertAlmostEqual(base.wavelength_to_energy(lam),
                                self.mono.energy)
+
+        # Wavelength-based monochromator.
+        self.wave_mono.wavelength = lam
+        self.assertAlmostEqual(self.wave_mono.wavelength, lam)
+        self.assertAlmostEqual(base.wavelength_to_energy(lam),
+                               self.wave_mono.energy)
+
+        # Useless monochromator.
+        with ShouldRaise(NotImplementedError):
+            self.useless_mono.wavelength = lam
