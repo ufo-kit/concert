0c10276e7eae0f2bf994938e311a6033891d8b32
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2013-05-02 12:40:16 +0200

    Add tomo scanner test

--- concert/tests/test_scan.py
+++ concert/tests/test_scan.py
@@ -2,9 +2,12 @@ import time
 import unittest
 import logbook
 import quantities as q
+from testfixtures import compare
 from concert.devices.motors.base import LinearCalibration
 from concert.devices.motors.dummy import Motor as DummyMotor
-from concert.processes.scan import Scanner, ascan, dscan
+from concert.devices.cameras.dummy import Camera
+from concert.processes.scan import Scanner, StepTomoScanner, ascan, dscan
+from concert.tests import slow
 
 
 def compare_sequences(first_sequence, second_sequence, assertion):
@@ -55,3 +58,41 @@ class TestScan(unittest.TestCase):
         scanner.intervals = 10
         x, y = scanner.run().result()
         compare_sequences(x, y, self.assertEqual)
+
+    @slow
+    def test_tomo_scanner(self):
+        class Stage(object):
+            def __init__(self):
+                self.angles = []
+                self.angle = 0.0 * q.deg
+
+            def move(self, value):
+                self.angle += value
+                self.angles.append(self.angle)
+
+        class PrepareChecker(object):
+            def __init__(self):
+                self.prepared = []
+
+            def prepare_dark_scan(self):
+                self.prepared.append('dark')
+
+            def prepare_flat_scan(self):
+                self.prepared.append('flat')
+
+            def prepare_proj_scan(self):
+                self.prepared.append('proj')
+
+        camera = Camera()
+        stage = Stage()
+        checker = PrepareChecker()
+        scanner = StepTomoScanner(camera, stage,
+                                  checker.prepare_dark_scan,
+                                  checker.prepare_flat_scan,
+                                  checker.prepare_proj_scan)
+
+        scanner.angle = 0.5 * q.deg
+
+        darks, flats, projections = scanner.run().result()
+        self.assertEqual(len(projections), len(stage.angles))
+        compare(checker.prepared, ['dark', 'flat', 'proj'])
