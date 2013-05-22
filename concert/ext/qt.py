72c0b912b6c673bbfa9c3b980089d56f999a33fe
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2013-05-22 12:38:32 +0200

    Add Qt widget creator

---
+++ concert/ext/qt.py
@@ -0,0 +1,24 @@
+from PyQt4 import QtGui
+
+
+def make(device):
+    """Create a Qt widget off of a *device*."""
+    layout = QtGui.QGridLayout()
+
+    for row, param in enumerate(device):
+        name_widget = QtGui.QLabel(str(param.name))
+        layout.addWidget(name_widget, row, 0)
+
+        value = param.get().result()
+        value_widget = None
+
+        if isinstance(value, str):
+            value_widget = QtGui.QLabel()
+            value_widget.setText(value)
+        else:
+            value_widget = QtGui.QDoubleSpinBox()
+            value_widget.setValue(value)
+
+        layout.addWidget(value_widget, row, 1)
+
+    return layout
