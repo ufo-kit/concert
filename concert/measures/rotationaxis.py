Rotation axis measure holes on edges fix.

concert/measures/rotationaxis.py
@@ -1,6 +1,6 @@
 """
 The module determines a 3D circle normal inclination angles towards the
-:math:`y`-axis (0,1,0) and the ellipse_center of the circle.
+:math:`y`-axis (0,1,0) and the center of the circle.
 
 **Algorithm**
 
@@ -63,7 +63,7 @@ class Ellipse(object):
         self._phi = None
         # ZY angle.
         self._psi = None
-        # Ellipse ellipse_center.
+        # Ellipse center.
         self._center = None
         # Ellipse parameters.
         self._params = None
@@ -93,8 +93,8 @@ class Ellipse(object):
         self._images = images
 
     @property
-    def ellipse_center(self):
-        """Ellipse ellipse_center (y, x)."""
+    def center(self):
+        """Ellipse center (y, x)."""
         if self._center is None:
             self._determine_center()
 
@@ -142,7 +142,7 @@ class Ellipse(object):
                 raise ValueError("No sample tip points found.")
 
     def _determine_center(self):
-        """Determine the ellipse_center of the ellipse from its parameters."""
+        """Determine the center of the ellipse from its parameters."""
         if self._params is None:
             self._fit_ellipse()
 
@@ -156,10 +156,11 @@ class Ellipse(object):
                      2*self._params[0]*self._params[4]) /\
                 (4*self._params[0]*self._params[2] - self._params[1]**2)
         else:
-            # We are not dealing with an ellipse, use ellipse_center of mass.
+            # We are not dealing with an ellipse, use center of mass.
             y_pos, x_pos = center_of_mass(self._tips)
 
-        self._center = y_pos, x_pos
+        # +1 for image edge-clipping compensation.
+        self._center = y_pos + 1, x_pos + 1
 
     def _determine_angles(self):
         """
@@ -284,7 +285,7 @@ def _get_intersection_points(image):
 def _get_axis_intersection(p_1, p_2, shape):
     """Get intersections of a vector perpendicular to a vector defined by
     *p_1* and *p_2* and image edges defined by image *shape*."""
-    # First check if the ellipse_center lies on an edge
+    # First check if the center lies on an edge
     if p_1[0] == p_2[0]:
         return [(p_1[0], (p_1[1] + p_2[1])/2)]
     elif p_1[1] == p_2[1]:
@@ -381,7 +382,8 @@ def _get_sample(image):
     """Segment the *image* to a sample and a background."""
     labels = _get_regions(image)
     labels = _get_boundary_regions(labels)
-    return _get_biggest_region(labels)
+    # Cut the edges by one pixel because hole filling does not affect them.
+    return _get_biggest_region(labels)[1:-1,1:-1]
 
 
 def _get_ellipse_points(images):
@@ -437,7 +439,7 @@ def _get_regions(image):
 
 
 def center_of_mass(points):
-    """Find the ellipse_center of mass from a set of *points*."""
+    """Find the center of mass from a set of *points*."""
     y_ind, x_ind = zip(*points)
 
     c_y = float(np.sum(y_ind))/len(points)

concert/tests/test_rotationaxismeasure.py
@@ -24,7 +24,7 @@ class TestRotationAxisMeasure(unittest.TestCase):
         self.z_motor["position"].unit = q.deg
 
         # The bigger the image size, the more images we need to determine
-        # the ellipse_center correctly.
+        # the center correctly.
         self.image_source = SimulationCamera(256, self.x_motor["position"],
                                              self.y_motor["position"],
                                              self.z_motor["position"])
@@ -57,9 +57,9 @@ class TestRotationAxisMeasure(unittest.TestCase):
         assert np.abs(psi) - np.abs(z_angle) < self.eps
 
     def center_check(self):
-        assert np.abs(self.measure.ellipse_center[1] -
+        assert np.abs(self.measure.center[1] -
                       self.image_source.ellipse_center[1]) < 1
-        assert np.abs(self.measure.ellipse_center[0] -
+        assert np.abs(self.measure.center[0] -
                       self.image_source.ellipse_center[0]) < 1
 
     @slow

concert/tests/util/rotationaxis.py
@@ -150,7 +150,7 @@ class SimulationCamera(Camera):
 
     @property
     def ellipse_center(self):
-        """Ellipse ellipse_center."""
+        """Ellipse center."""
         return self._center
 
     def create_needle(self):
@@ -164,7 +164,7 @@ class SimulationCamera(Camera):
 
         center = np.dot(np.linalg.inv(matrix),
                         (0, self.size/8/self.scale[1], 0, 1))+self.size/2
-        # Ellipse ellipse_center.
+        # Ellipse center.
         self._center = center[1], center[0]
 
         matrix = rot_y(self.y_axis_param.get().result(), matrix)