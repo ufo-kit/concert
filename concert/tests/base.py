bd9aa7c3a23b8cde66b26b3ce579babf20eb2196
Tomas Farago <tomas.farago@kit.edu>
2013-08-29 08:42:51 +0200

    Logging suppression decorator for tests

--- concert/tests/base.py
+++ concert/tests/base.py
@@ -2,6 +2,19 @@ import logbook
 from unittest import TestCase
 
 
+def suppressed_logging(func):
+    """Decorator for test functions."""
+    def test_wrapper(*args, **kwargs):
+        handler = logbook.NullHandler()
+        handler.push_application()
+
+        func(*args, **kwargs)
+
+        handler.pop_application()
+
+    return test_wrapper
+
+
 class ConcertTest(TestCase):
 
     def setUp(self):
