ca7e16e937c386efd94f4a24e3f1e018fd89252c
Tomas Farago <tomas.farago@kit.edu>
2013-09-11 17:49:31 +0200

    Adventurer scales

---
+++ concert/devices/scales/adventurer.py
@@ -0,0 +1,51 @@
+"""Adventurer scales."""
+
+from concert.devices.scales import base
+from concert.devices.base import LinearCalibration, Device
+from concert.connections.inet import Connection
+from concert.quantities import q
+
+
+class AdventurerError(Exception):
+
+    """Error specific for Adventurer scales."""
+    pass
+
+
+class ARRW60(base.TarableScales):
+
+    """The ARRW60 model of Adventurer scales."""
+    ERROR = "error"
+    OK = "ok"
+
+    def __init__(self, host, port):
+        super(ARRW60, self).__init__(LinearCalibration(q.counts / q.g,
+                                                       0 * q.g))
+        self._connection = Connection(host, port, "\r\n")
+        self._states = self._states.union(set([ARRW60.OK, ARRW60.ERROR]))
+
+    def _execute(self, cmd):
+        result = self._connection.execute(cmd)
+        if "OK!" not in result:
+            raise AdventurerError("Bad command or value")
+
+    def _tare(self):
+        self._execute("T")
+
+    def _get_weight(self):
+        """
+        Get weight from the scales. The command does not return until
+        a balanced position is found, thus it can time out.
+        """
+        res = self._connection.execute("P")
+        if "Err8.4" in res:
+            self._set_state(ARRW60.ERROR)
+            raise AdventurerError("More than maximum weight loaded")
+        else:
+            if self.state == ARRW60.ERROR or self.state == Device.NA:
+                # Clear the error from before or set OK for the first time
+                self._set_state(ARRW60.OK)
+            # The returned string contains units
+            res = q.Quantity(res).to(q.g)
+
+        return float(res.magnitude) * q.counts
