1385a01f2d124d6736320f402c3b0f814c896f1c
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2013-10-09 09:37:30 -0700

    Move commands to the concert script
    It does not make much sense to export the functions as part of the API, hence
    everything is moved to the script itself. To allow easier extension later on
    every command as been turned into an object.

--- concert/helpers.py
+++ concert/helpers.py
@@ -172,3 +172,23 @@ def broadcast(*consumers):
         item = yield
         for consumer in consumers:
             consumer.send(item)
+
+
+class Command(object):
+    """Command class for the CLI script"""
+
+    def __init__(self, name, opts):
+        """
+        Command objects are loaded at run-time and injected into Concert's
+        command parser.
+
+        *name* denotes the name of the sub-command parser, e.g. "mv" for the
+        MoveCommand. *opts* must be an argparse-compatible dictionary
+        of command options.
+        """
+        self.name = name
+        self.opts = opts
+
+    def run(self, *args, **kwargs):
+        """Run the command"""
+        raise NotImplementedError
