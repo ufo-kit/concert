170a65db67edbb61c3ebf3bebbdc86a4fc10fdbb
Tomas Farago <tomas.farago@kit.edu>
2013-10-26 13:05:35 +0200

    Drop camera specific functions

--- concert/experiments/imaging.py
+++ concert/experiments/imaging.py
@@ -1,5 +1,4 @@
 """Imaging experiments usually conducted at synchrotrons."""
-import time
 import os
 from concert.storage import write_tiff, create_folder
 from concert.coroutines import write_images, ImageAverager, flat_correct
@@ -8,72 +7,6 @@ from concert.helpers import inject, broadcast
 DARKS = "darks"
 FLATS = "flats"
 RADIOS = "radios"
-
-
-def acquire_images_dummy(num_images, images, value=None):
-    """
-    Acquire *num_images* and store them in *images*. If *value*
-    is provided, the images will have every pixel set to *value*,
-    otherwise the pixels are filled with random numbers.
-    """
-    import numpy as np
-
-    for i in range(num_images):
-        if value is None:
-            image = np.random.random(size=(512, 512))
-        else:
-            image = np.ones((512, 512)) * value
-        images.append(image.astype(np.float32))
-
-
-def readout_images_dummy(images):
-    """Readout *images* in a generator."""
-    for image in images:
-        yield image
-
-
-def take_dummy_continuous(num_images, value=None):
-    """
-    Take *num_images* of dummy images filled with *value* if not None,
-    otherwise random numbers.
-    """
-    images = []
-    acquire_images_dummy(num_images, images, value=value)
-    for image in readout_images_dummy(images):
-        yield image
-
-
-def acquire_images(camera, num_images):
-    """Acquire *num_images* with a libuca *camera*."""
-    try:
-        camera.trigger_mode = camera.uca.props.trigger_mode.AUTO
-        camera.start_recording()
-        time.sleep(num_images / camera.frames_per_second)
-    finally:
-        camera.stop_recording()
-
-
-def readout_images(camera):
-    """Readout images from libuca *camera* in a generator."""
-    num_frames = camera.recorded_frames.magnitude
-
-    try:
-        camera.start_readout()
-
-        for i in xrange(num_frames):
-            frame = camera.grab()
-
-            if frame is not None:   # double check
-                yield frame
-    finally:
-        camera.stop_readout()
-
-
-def take_continuous(camera, num_images):
-    """Take *num_images* continuously using a libuca *camera*."""
-    acquire_images(camera, num_images)
-    for image in readout_images(camera):
-        yield image
 
 
 def add_writer(folder, take_images, process_images, image_type,
