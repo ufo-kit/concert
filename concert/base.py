71890089d555c27b52a9ba91107b6c7970096651
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2013-11-15 16:23:48 +0100

    Move device docs to rst

--- concert/base.py
+++ concert/base.py
@@ -1,46 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-A *device* is a software abstraction for a piece of hardware that can be
-controlled.
-
-Each device consists of a set of named :class:`Parameter` instances and
-device-specific methods. If you know the parameter name, you can get
-a reference to the parameter object by using the index operator::
-
-    pos_parameter = motor['position']
-
-To set and get parameters explicitly , you can use the :meth:`Parameter.get`
-and :meth:`Parameter.set` methods::
-
-    pos_parameter.set(1 * q.mm)
-    print (pos_parameter.get().result())
-
-Both methods will return a *Future*. A future is a promise that a result will
-be delivered when asked for. In the mean time other things can and should
-happen concurrently. As you can see, to get the result of a future you call its
-``result()`` method.
-
-An easier way to set and get parameter values are properties via the
-dot-name-notation::
-
-    motor.position = 1 * q.mm
-    print (motor.position)
-
-As you can see, accessing parameters this way will *always be synchronous* and
-*block* execution until the value is set or fetched.
-
-Parameter objects are not only used to communicate with a device but also carry
-meta data information about the parameter. The most important ones are
-:attr:`Parameter.name`, :attr:`Parameter.unit` and
-:attr:`Parameter.in_hard_limit` as well as the doc string describing the
-parameter. Moreover, parameters can be queried for access rights using
-:meth:`Parameter.is_readable` and :meth:`Parameter.is_writable`.
-
-To get all parameters of an object, you can iterate over the device itself ::
-
-    for param in motor:
-        print("{0} => {1}".format(param.unit, param.name))
-"""
+"""Core module Parameters"""
 import re
 import logging
 from concert.helpers import dispatcher, async, wait
