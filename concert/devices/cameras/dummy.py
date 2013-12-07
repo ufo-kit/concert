54eb450f55001c35d09da2a33f2b691f9a05a1f2
Tomas Farago <tomas.farago@kit.edu>
2013-12-07 17:16:44 +0100

    Refactor dummy Camera classes

--- concert/devices/cameras/dummy.py
+++ concert/devices/cameras/dummy.py
@@ -8,114 +8,55 @@ import time
 from concert.storage import read_image
 
 
-class FileCamera(base.Camera):
-
-    """A camera that reads files in a *directory*."""
-
-    def __init__(self, directory):
-        # Let users change the directory
-        self.directory = directory
-        super(FileCamera, self).__init__()
-
-        self._frame_rate = 1000 * q.count / q.s
-        self._trigger_mode = self.trigger_modes.AUTO
-        self._index = 0
-        self.roi_x = 0
-        self.roi_y = 0
-        self.roi_width = None
-        self.roi_height = None
-        self._files = [os.path.join(directory, file_name) for file_name in
-                       sorted(os.listdir(directory))]
-
-    def _get_frame_rate(self):
-        return self._frame_rate
-
-    def _set_frame_rate(self, frame_rate):
-        self._frame_rate = frame_rate
-
-    def _get_trigger_mode(self):
-        return self._trigger_mode
-
-    def _set_trigger_mode(self, mode):
-        self._trigger_mode = mode
-
-    def _record_real(self):
-        pass
-
-    def _stop_real(self):
-        pass
-
-    def _trigger_real(self):
-        pass
-
-    def _grab_real(self):
-        if self._index < len(self._files):
-            image = read_image(self._files[self._index])
-
-            if self.roi_height is None:
-                y_region = image.shape[0]
-            else:
-                y_region = self.roi_y + self.roi_height
-
-            if self.roi_width is None:
-                x_region = image.shape[1]
-            else:
-                x_region = self.roi_x + self.roi_width
-
-            result = image[self.roi_y:y_region, self.roi_x:x_region]
-            self._index += 1
-        else:
-            result = None
-
-        return result
-
-
-class Camera(base.Camera):
-
-    """Simple camera.
-
-    *background* can be an array-like that will be used to generate the frame
-    when calling :meth:`.grab`. The final image will be the background +
-    poisson depending on the currently set exposure time.
-
-    .. py:attribute:: exposure_time
-    .. py:attribute:: roi_width
-
-        Width of the image returned by :meth:`.grab`.
-
-    .. py:attribute:: roi_height
-
-        Height of the image returned by :meth:`.grab`.
-
-    .. py:attribute:: sensor_pixel_width
-    .. py:attribute:: sensor_pixel_height
-    """
+class Base(base.Camera):
 
     exposure_time = Quantity(unit=q.s)
     sensor_pixel_width = Quantity(unit=q.micrometer)
     sensor_pixel_height = Quantity(unit=q.micrometer)
+    roi_x0 = Quantity(unit=q.dimensionless)
+    roi_y0 = Quantity(unit=q.dimensionless)
+    roi_width = Quantity(unit=q.dimensionless)
+    roi_height = Quantity(unit=q.dimensionless)
 
-    def __init__(self, background=None):
-        super(Camera, self).__init__()
-
-        self._frame_rate = 10.0 / q.s
+    def __init__(self):
+        super(Base, self).__init__()
+        self._frame_rate = 1000 / q.s
         self._trigger_mode = self.trigger_modes.AUTO
         self._exposure_time = 1 * q.ms
-
-        if background is not None:
-            self.roi_width = background.shape[1]
-            self.roi_height = background.shape[0]
-            self._background = background
-        else:
-            shape = (640, 480)
-            self.roi_width, self.roi_height = shape
-            self._background = np.ones(shape)
+        self._roi_x0 = 0
+        self._roi_y0 = 0
+        self._roi_width = 640
+        self._roi_height = 480
 
     def _get_sensor_pixel_width(self):
         return 5 * q.micrometer
 
     def _get_sensor_pixel_height(self):
         return 5 * q.micrometer
+
+    def _get_roi_x0(self):
+        return self._roi_x0
+
+    def _set_roi_x0(self, x0):
+        self._roi_x0 = x0
+
+    def _get_roi_y0(self):
+        return self._roi_y0
+
+    def _set_roi_y0(self, y0):
+        self._roi_y0 = y0
+
+    def _get_roi_width(self):
+        return self._roi_width
+
+    def _set_roi_width(self, roi):
+        self._roi_width = roi
+
+    def _get_roi_height(self):
+        return self._roi_height
+
+    def _set_roi_height(self, roi):
+        self._roi_height = roi
 
     def _get_exposure_time(self):
         return self._exposure_time
@@ -144,6 +85,29 @@ class Camera(base.Camera):
     def _trigger_real(self):
         pass
 
+
+class Camera(Base):
+
+    """Simple camera.
+
+    *background* can be an array-like that will be used to generate the frame
+    when calling :meth:`.grab`. The final image will be the background +
+    poisson noise depending on the currently set exposure time.
+
+   """
+
+    def __init__(self, background=None):
+        super(Camera, self).__init__()
+
+        if background is not None:
+            self.roi_width = background.shape[1]
+            self.roi_height = background.shape[0]
+            self._background = background
+        else:
+            shape = (640, 480)
+            self.roi_width, self.roi_height = shape
+            self._background = np.ones(shape)
+
     def _grab_real(self):
         start = time.time()
         cur_time = self.exposure_time.to(q.s).magnitude
@@ -160,6 +124,43 @@ class Camera(base.Camera):
             time.sleep(to_sleep.magnitude)
 
         return np.cast[np.uint16](tmp)
+
+
+class FileCamera(Base):
+
+    """A camera that reads files in a *directory*."""
+
+    def __init__(self, directory):
+        # Let users change the directory
+        self.directory = directory
+        super(FileCamera, self).__init__()
+
+        self._index = 0
+        self.roi_width = None
+        self.roi_height = None
+        self._files = [os.path.join(directory, file_name) for file_name in
+                       sorted(os.listdir(directory))]
+
+    def _grab_real(self):
+        if self._index < len(self._files):
+            image = read_image(self._files[self._index])
+
+            if self.roi_height is None:
+                y_region = image.shape[0]
+            else:
+                y_region = self.roi_y + self.roi_height
+
+            if self.roi_width is None:
+                x_region = image.shape[1]
+            else:
+                x_region = self.roi_x + self.roi_width
+
+            result = image[self.roi_y:y_region, self.roi_x:x_region]
+            self._index += 1
+        else:
+            result = None
+
+        return result
 
 
 class BufferedCamera(Camera, base.BufferedMixin):
