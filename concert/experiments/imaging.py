951a71367124804cb71a13488b79d7b46e88881b
Tomas Farago <tomas.farago@kit.edu>
2013-12-12 15:07:31 +0100

    Add tomography helpers

--- concert/experiments/imaging.py
+++ concert/experiments/imaging.py
@@ -2,6 +2,8 @@
 """Imaging experiments usually conducted at synchrotrons."""
 import os
 import logging
+import numpy as np
+from concert.quantities import q
 from concert.storage import write_tiff, create_directory
 from concert.coroutines.sinks import write_images
 from concert.experiments.base import Experiment as BaseExperiment
@@ -64,3 +66,40 @@ class Experiment(BaseExperiment):
         for acq, launcher in self._writers.items():
             acq.consumers.remove(launcher)
         self._writers = {}
+
+
+def get_angular_step(frame_width):
+    """
+    Get the angular step required for tomography so that every pixel of the frame
+    rotates no more than one pixel per rotation step. *frame_width* is frame size in
+    the direction perpendicular to the axis of rotation.
+    """
+    return np.arctan(2.0 / frame_width)
+
+
+def get_tomo_projections_number(frame_width):
+    """
+    Get the minimum number of projections required by a tomographic scan in
+    order to provide enough data points for every distance from the axis of
+    rotation. The minimum angular step is
+    considered to be needed smaller than one pixel in the direction
+    perpendicular to the axis of rotation. The number of pixels in this
+    direction is given by *frame_width*.
+    """
+    return int(np.ceil(np.pi / get_angular_step(frame_width).magnitude))
+
+
+def get_tomo_max_speed(frame_width, frame_rate):
+    """
+    Get the maximum rotation speed which introduces motion blur less than one
+    pixel. *frame_width* is the width of the frame in the direction
+    perpendicular to the rotation and *frame_rate* defines the time required
+    for recording one frame.
+
+    _Note:_ frame rate is required instead of exposure time because the
+    exposure time is usually shorter due to the camera chip readout time.
+    We need to make sure that by the next exposure the sample hasn't moved
+    more than one pixel from the previous frame, thus we need to take into
+    account the whole frame taking procedure (exposure + readout).
+    """
+    return get_angular_step(frame_width) * frame_rate
