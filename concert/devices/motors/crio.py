0c3508fa3e604eb075d5cff01f5075a6a66b04d0
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2013-12-12 14:08:46 +0100

    Update CompactRIO based motors

--- concert/devices/motors/crio.py
+++ concert/devices/motors/crio.py
@@ -1,91 +1,63 @@
 """
-Axes based on the CompactRIO controller.
+Motors based on the CompactRIO controller.
 """
-import readline
 from concert.quantities import q
-from concert.devices.base import LinearCalibration
-from concert.devices.motors.base import Motor
+from concert.base import HardLimitError
 from concert.networking import SocketConnection
+from concert.devices.motors import base
 
 
-CRIO_HOST = 'cRIO9074-Motion.ka.fzk.de'
-CRIO_PORT = 6342
+DEFAULT_CRIO_HOST = 'cRIO9074-Motion.ka.fzk.de'
+DEFAULT_CRIO_PORT_LINEAR = 6342
+DEFAULT_CRIO_PORT_ROTATIONAL = 6340
 
 
-class LinearMotor(Motor):
+class LinearMotor(base.LinearMotor):
 
     """A linear motor that moves in two directions."""
 
-    def __init__(self):
-        calibration = LinearCalibration(50000 / q.mm, -1 * q.mm)
-        super(LinearMotor, self).__init__(calibration)
+    def __init__(self, host=DEFAULT_CRIO_HOST, port=DEFAULT_CRIO_PORT_LINEAR):
+        super(LinearMotor, self).__init__()
 
-        self._connection = SocketConnection(CRIO_HOST, CRIO_PORT)
-        self['position'].limiter = lambda x: x >= 0 * q.mm and x <= 2 * q.mm
-
-        self._home()
+        self._connection = SocketConnection(host, port, '\r\n')
         self._steps = 0
+        self._to_device_scale = 50000 / q.mm
+        self['position'].lower = 0 * q.mm
+        self['position'].upper = 2 * q.mm
+
+    def in_hard_limit(self):
+        return self._steps < 0 or self._steps > 100000
 
     def _get_position(self):
-        return self._steps
+        return self._steps / self._to_device_scale
 
-    def _set_position(self, steps):
-        self._connection.send('lin %i\r\n' % steps)
-        self._connection.recv()
-        self._steps = steps
-
-    def _stop(self):
-        pass
-
-    def _home(self):
-        self._set_position(0)
+    def _set_position(self, position):
+        steps = position * self._to_device_scale
+        self._connection.execute('lin {} 5000'.format(int(steps)))
 
 
-class RotationMotor(Motor):
+class RotationMotor(base.ContinuousRotationMotor):
 
     """A rotational motor."""
 
-    def __init__(self):
-        calibration = LinearCalibration(50000 / q.mm, 0 * q.mm)
-        super(RotationMotor, self).__init__(calibration)
-
+    def __init__(self, host=DEFAULT_CRIO_HOST, port=DEFAULT_CRIO_PORT_ROTATIONAL):
+        super(RotationMotor, self).__init__()
+        self._connection = SocketConnection(host, port, '\r\n')
         self._steps = 0
-        self._connection = SocketConnection(CRIO_HOST, CRIO_PORT)
-        self['position'].limiter = lambda x: x >= 0 * q.mm and x <= 2 * q.mm
+        self._to_device_scale = 2 / q.deg
+
+    def in_hard_limit(self):
+        return False
 
     def _get_position(self):
-        return self._steps
+        return self._steps / self._to_device_scale
 
-    def _set_position(self, steps):
-        self._steps = steps
-        self._connection.send('rot %i\r\n' % steps)
-        self._connection.recv()
+    def _set_position(self, position):
+        steps = position * self._to_device_scale
+        self._connection.execute('rot {} 500'.format(int(steps)))
 
-    def _stop(self):
+    def _get_velocity(self):
+        return 0 * q.deg / q.s
+
+    def _set_velocity(self, velocity):
         pass
-
-
-def main():
-    """Main definition."""
-    readline.parse_and_bind('tab: complete')
-
-    linear_device = LinearMotor()
-    rotation_device = RotationMotor()
-
-    while True:
-        line = raw_input('> ')
-        if line == 'q':
-            break
-
-        try:
-            command, value = line.split()
-            if command == 'r':
-                rotation_device.set_position(float(value) * q.mm)
-            elif command == 'm':
-                linear_device.set_position(float(value) * q.mm)
-        except ValueError:
-            print("Commands: `r [NUM]', `m [NUM]', `q'")
-
-
-if __name__ == '__main__':
-    main()
