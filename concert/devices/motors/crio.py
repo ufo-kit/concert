3c3989bdc28cbf2cc65b95f56bd11c4b4707754f
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2013-12-16 12:45:21 +0100

    Continuous rotation on the cRIO motor

--- concert/devices/motors/crio.py
+++ concert/devices/motors/crio.py
@@ -18,14 +18,13 @@ class LinearMotor(base.LinearMotor):
 
     def __init__(self, host=DEFAULT_CRIO_HOST, port=DEFAULT_CRIO_PORT_LINEAR):
         super(LinearMotor, self).__init__()
-
         self._connection = SocketConnection(host, port, '\r\n')
         self._steps = 0
         self._to_device_scale = 50000 / q.mm
         self['position'].lower = 0 * q.mm
         self['position'].upper = 2 * q.mm
 
-    def in_hard_limit(self):
+    def _in_hard_limit(self):
         return self._steps < 0 or self._steps > 100000
 
     def _get_position(self):
@@ -34,6 +33,9 @@ class LinearMotor(base.LinearMotor):
     def _set_position(self, position):
         steps = position * self._to_device_scale
         self._connection.execute('lin {} 5000'.format(int(steps)))
+
+    def _stop(self):
+        self._connection.execute('mod stop')
 
 
 class RotationMotor(base.ContinuousRotationMotor):
@@ -44,9 +46,13 @@ class RotationMotor(base.ContinuousRotationMotor):
         super(RotationMotor, self).__init__()
         self._connection = SocketConnection(host, port, '\r\n')
         self._steps = 0
+        self._velocity = 0
         self._to_device_scale = 2 / q.deg
 
-    def in_hard_limit(self):
+    def _in_hard_limit(self):
+        return False
+
+    def _in_velocity_hard_limit(self):
         return False
 
     def _get_position(self):
@@ -60,4 +66,8 @@ class RotationMotor(base.ContinuousRotationMotor):
         return 0 * q.deg / q.s
 
     def _set_velocity(self, velocity):
-        pass
+        step_velocity = velocity * self._to_device_scale * q.s
+        self._connection.execute('mod {}'.format(int(step_velocity)))
+
+    def _stop(self):
+        self._connection.execute('mod stop')
