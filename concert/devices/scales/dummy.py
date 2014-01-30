77bbd1eac0fc1d3ff7d003a82c77d12b08391672
Tomas Farago <tomas.farago@kit.edu>
2014-01-30 17:02:42 +0100

    Added dummy scales

---
+++ concert/devices/scales/dummy.py
@@ -0,0 +1,17 @@
+"""Dummy scales."""
+from concert.quantities import q
+from concert.devices.scales import base
+
+
+class Scales(base.Scales):
+    def __init__(self):
+        super(Scales, self).__init__()
+        self._weight = 147
+
+    def _get_weight(self):
+        return self._weight
+
+
+class TarableScales(Scales):
+    def _tare():
+        pass
