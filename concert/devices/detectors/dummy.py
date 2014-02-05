5633a5832fb7fece92b13a0b26b3b8a226086fc7
Tomas Farago <tomas.farago@kit.edu>
2014-02-05 15:15:42 +0100

    Added dummy detector

---
+++ concert/devices/detectors/dummy.py
@@ -0,0 +1,11 @@
+"""A dummy detector."""
+from concert.devices.detectors import base
+from concert.devices.cameras.dummy import Camera
+
+
+class Detector(base.Detector):
+    """Dummy detector."""
+    def __init__(self, camera=None, magnification=None):
+        camera = Camera() if camera is None else camera
+        magnification = 3 if magnification is None else magnification
+        super(Detector, self).__init__(camera, magnification)
