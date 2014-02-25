c590081470e6916553ff4a00f7427e203faff0b4
Tomas Farago <tomas.farago@kit.edu>
2014-02-25 09:10:08 +0100

    Refactor Aerotech HLe IO device
    Rename because the ports number is different for different
    Aerotech controllers.

--- concert/devices/io/aerotech.py
+++ concert/devices/io/aerotech.py
@@ -1,21 +1,24 @@
-"""Aerotech"""
+"""Aerotech controllers IO boards implementation."""
+from concert.networking import Aerotech
 from concert.devices.io import base
-from concert.devices.io.base import Port
 
 
-class IO(base.IO):
+def _make_ids(index, num_items):
+    return zip([index] * num_items, range(num_items))
 
-    """IO class implementation."""
 
-    def __init__(self, connection):
-        ports = [Port((1, 0), "read", self.read_port, None, "Read port."),
-                 Port((1, 1), "write", None, self.write_port, "Write port.")]
-        super(IO, self).__init__(ports)
-        self._connection = connection
+class HLe(base.IO):
 
-    def read_port(self, port):
-        return self._connection.execute("DIN(X,%d,%d)" % (port[0], port[1]))
+    """Aerotech HLe controller IO board."""
 
-    def write_port(self, port, value):
-        self._connection.execute("DOUT(X,%d,%d:%d)" %
-                                 (port[0], port[1], value))
+    def __init__(self, host, port=8001):
+        super(HLe, self).__init__()
+        self._connection = Aerotech(host, port)
+        # Three groups, first 0-5, the rest 0-7
+        self._ports = _make_ids(0, 6) + _make_ids(1, 8) + _make_ids(2, 8)
+
+    def _read_port(self, port):
+        return int(self._connection.execute("DIN(X,%d,%d)" % (port[0], port[1])))
+
+    def _write_port(self, port, value):
+        self._connection.execute("DOUT(X,%d,%d:%d)" % (port[0], port[1], value))
