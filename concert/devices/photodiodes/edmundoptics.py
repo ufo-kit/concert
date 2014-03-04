727d2e965368278d412beb00d18492c05057fb3d
Tomas Farago <tomas.farago@kit.edu>
2014-03-04 16:37:29 +0100

    Added EdmundOptics photodiode

---
+++ concert/devices/photodiodes/edmundoptics.py
@@ -0,0 +1,25 @@
+"""EdmundOptics photodiode implementation."""
+
+from concert.quantities import q
+from concert.devices.io.wago import IO
+from concert.devices.photodiodes import base
+
+
+class PhotoDiode(base.PhotoDiode):
+
+    """
+    Impementation of Edmund Optics photodiode with V/W output signal
+    """
+
+    def __init__(self, host, port):
+        self._host = host
+        self._port = port
+        super(PhotoDiode, self).__init__()
+
+    def _get_intensity(self, port):
+        """
+        Read output intensity from the photodiode
+        """
+        connection = IO(self._host, self._port)
+        intensity = float(connection._read_port(port)) / 100
+        return intensity * q.V / q.W
