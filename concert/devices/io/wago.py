d701cc4d9ceaa5e590781ff55aedc2cb80733ce6
ALiOz <ali.ozdiev95@gmail.com>
2014-03-05 15:52:29 +0100

    Specified wago ports

--- concert/devices/io/wago.py
+++ concert/devices/io/wago.py
@@ -11,7 +11,7 @@ class Wago(Device):
     def __init__(self, host, port):
         self._host = host
         self._port = port
-        self._ports = {}
+        self._ports = {0:0, 1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, 10:10, 11:11, 12:12}
 
     def _write_port(self, port, value):
         """Set input value"""
