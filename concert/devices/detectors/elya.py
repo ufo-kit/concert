ac488c1e3bc8c3cb53ab0b883b636d9f694e1b80
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2014-03-07 14:10:25 +0100

    Implement selection of cameras and scintillators

--- concert/devices/detectors/elya.py
+++ concert/devices/detectors/elya.py
@@ -1,44 +1,31 @@
+from concert.base import Selection
 from concert.devices.detectors import base
 
 
 class Detector(base.Detector):
 
-    camera = Selection([1, 2, 3])
-    magnification = Range(1.0, 4.0)
-    filter = Selection([1, 2, 3, 4])
+    scintillator = Selection(list(range(1, 12)), help="Current scintillator")
+    use = Selection([1, 2, 3], help="Current camera in use")
 
-    def __init__(self, cameras=None):
-        super(Detector, self).__init__()
+    def __init__(self, cameras):
+        if len(cameras) < 3:
+            raise ValueError("You must pass at least three cameras")
 
-        # These are concert.devices.cameras.* objects
+        super(Detector, self).__init__(cameras[0], 2.0)
+
         self._cameras = cameras
+        self._current_camera = 1
+        self._current_scintillator = 1
 
-    def current(self):
-        """Return currently selected camera object."""
+    def _set_use(self, value):
+        self._current_camera = value
+        self._camera = self._cameras[value - 1]
 
-        # This could (?) come in pretty handy as in
-        #   
-        #   with detector.current().recording():
-        #       frames = acquire(detector.current())
-        #
-        return self._cameras(self.camera.index - 1)
+    def _get_use(self):
+        return self._current_camera
 
-    def _set_camera(self, value):
-        # rotate the dish washer
-        pass
+    def _set_scintillator(self, value):
+        self._current_scintillator = value
 
-    def _get_camera(self):
-        pass
-
-    def _set_magnification(self, value):
-        # adjust the lens
-        pass
-
-    def _get_magnification(self):
-        pass
-
-    def _set_filter(self, value):
-        pass
-
-    def _get_filter(self, value):
-        pass
+    def _get_scintillator(self):
+        return self._current_scintillator
