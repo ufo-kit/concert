82c78ec117bcf8a5f164405fcfe96cf10a68dbcf
roslin13 <07.roslin@gmail.com>
2014-03-31 16:21:31 +0200

    Modified method names

--- concert/devices/cooldevice.py
+++ concert/devices/cooldevice.py
@@ -1,18 +1,28 @@
-devices.motors.base import RotationMotor 
+"""Filter selection"""
+from concert.devices.motors.dummy import RotationMotor
 from concert.devices.base import Device
+from concert.base import Selection
+from concert.quantities import q
+
 
 class Lightfilter(Device):
+    
+    """Filter selection motor"""
+  
+    filtermotor = Selection(list(range(1, 6)))
 
     def __init__(self):
-        #filtermotor = Selection(list(range(1, 6)))
         super(Lightfilter, self).__init__()
-        self._filter_motor = RotationMotor()
-    
+        self._filter_select = RotationMotor()
+        self._filter_slot = 0
+
     def _set_filter(self, value):
+        """Set the chosen filter"""
+        
         self._filter_slot = value
-        self._filter_motor.position = 360.0 / value * q.deg 
+        self._filter_select.position = 360.0 / value * q.deg
 
     def _get_filter(self):
+        """Give chosen filter"""
+        
         return self._filter_slot
-
-filt = Lightfilter
