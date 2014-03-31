3ff3e6acee49c6f443204531b7f0311398fe0b88
roslin13 <07.roslin@gmail.com>
2014-03-31 16:52:11 +0200

    Changed method and parameter name

--- concert/devices/cooldevice.py
+++ concert/devices/cooldevice.py
@@ -9,20 +9,22 @@ class Lightfilter(Device):
     
     """Filter selection motor"""
   
-    filtermotor = Selection(list(range(1, 6)))
+    filter_select = Selection(list(range(1, 6)))
 
     def __init__(self):
         super(Lightfilter, self).__init__()
-        self._filter_select = RotationMotor()
+        self._filter_motor = RotationMotor()
         self._filter_slot = 0
 
     def _set_filter(self, value):
+        
         """Set the chosen filter"""
         
         self._filter_slot = value
-        self._filter_select.position = 360.0 / value * q.deg
+        self._filter_motor.position = 360.0 / value * q.deg
 
     def _get_filter(self):
+        
         """Give chosen filter"""
         
         return self._filter_slot
