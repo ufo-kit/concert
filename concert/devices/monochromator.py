a148cdd182a9278bbdf22fcd696dcfb5dd237ad8
roslin13 <07.roslin@gmail.com>
2014-04-17 16:06:41 +0200

    Changed description of Monochromator class

--- concert/devices/monochromator.py
+++ concert/devices/monochromator.py
@@ -7,35 +7,31 @@ Created on Wed Apr 16 13:16:41 2014
 
 from concert.devices.motors.dummy import RotationMotor
 from concert.devices.base import Device
-from concert.base import Selection
 from concert.quantities import q
+from concert.base import Quantity
 
 class Monochromator(Device):
     
-    """Rotation of motors based on energy level"""
-  
-    motor = Selection(list(range(1, 44)))
-    #motor2 = Selection(list(range(1, 44)))
+    """Rotation of motors based on wavelength"""
+    
+    wavelength = Quantity(q.m)
 
     def __init__(self):
         super(Monochromator, self).__init__()
+        #self.wavelength = None * q.m
         self._motor1 = RotationMotor()
         self._motor2 = RotationMotor()
     
-        self._motor1_energy = 0 
-        #self._motor2_energy = 0
-        
-        
-    def _set_motor(self, value):
+    def set_wavelength(self, value):
         
         """Rotate the motors"""
-        
-        self._motor1_energy = value * q.keV
-        self._motor1.position = value * q.deg
-        self._motor2.position = value * q.deg
-
-    def _get_motor(self):
+        self.wavelength = value
+        self.magnitude = str(self.wavelength)
+        self._motor1.position = (float(self.magnitude.split(" ")[0]) * 1000) * q.deg
+        self._motor2.position = (float(self.magnitude.split(" ")[0]) * 1000) * q.deg
+  
+    def get_wavelength(self):
         
         """Give chosen energylevel"""
         
-        return self._motor1_energy
+        return self.wavelength
