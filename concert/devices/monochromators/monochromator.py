8fa0cbc004d01237f3dc665c633a77540b8f68a8
roslin13 <07.roslin@gmail.com>
2014-04-23 12:38:58 +0200

    Changed code again

---
+++ concert/devices/monochromators/monochromator.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 16 13:16:41 2014
+
+@author: francis
+"""
+
+from concert.devices.motors.dummy import RotationMotor
+from concert.devices.base import Device
+from concert.quantities import q
+from concert.base import Quantity
+
+
+class Monochromator(Device):
+
+    """Rotation of motors based on wavelength"""
+
+    wavelength = Quantity(q.m)
+
+    def __init__(self):
+        super(Monochromator, self).__init__()
+        self._motor1 = RotationMotor()
+        self._motor2 = RotationMotor()
+
+    def _set_wavelength(self, value):
+
+        """Rotate the motors"""
+
+        self.magnitude = str(value)
+        self._motor1.position = self.magnitude * q.deg
+        self._motor2.position = self.magnitude * q.deg
+
+    def _get_wavelength(self):
+
+        """Give chosen energylevel"""
+
+        return (self.magnitude)
