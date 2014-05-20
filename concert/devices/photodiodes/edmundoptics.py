78973e1b07d1fad866cbe988bf99298bc34d586c
ALiOz <ali.ozdiev95@gmail.com>
2014-05-20 11:19:28 +0200

    Corrected port initialization for EdmundOptics photodiode

--- concert/devices/photodiodes/edmundoptics.py
+++ concert/devices/photodiodes/edmundoptics.py
@@ -11,15 +11,16 @@ class PhotoDiode(base.PhotoDiode):
     Impementation of Edmund Optics photodiode with V output signal
     """
 
-    def __init__(self, host, port):
+    def __init__(self, host, port, wago_port):
         self._host = host
         self._port = port
+        self._wago_port = wago_port
         super(PhotoDiode, self).__init__()
 
-    def _get_intensity(self, port):
+    def _get_intensity(self):
         """
         Read output intensity from the photodiode
         """
         connection = IO(self._host, self._port)
-        intensity = float(connection._read_port(port)) / 1000
+        intensity = float(connection._read_port(self._wago_port)) / 1000
         return intensity * q.V
