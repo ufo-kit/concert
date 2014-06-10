d2af7b6f945a22884aaa5ef65df04103ad6a44c3
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2014-06-10 10:15:28 +0200

    Do not allow reconfigurable kiro camera

--- concert/devices/cameras/kiro.py
+++ concert/devices/cameras/kiro.py
@@ -4,19 +4,14 @@ from concert.devices.cameras import uca
 
 class Camera(base.Camera):
 
-    def __init__(self, ip=None, port=None):
+    def __init__(self, ip, port):
         super(Camera, self).__init__()
 
-        self._uca_camera = uca.Camera('kiro')
         self._tango_camera = None
+        # fetch properties from remote camera
 
-        if ip and port:
-            self.connect(ip, port)
-
-    def connect(self, ip, port):
+        self._uca_camera = uca.Camera('kiro')
         self._uca_camera.set_properties(ip=ip, port=port)
-        # Here we also need to get the properties from the tango device and map
-        # them to device properties.
 
     def _record_real(self):
         pass    # use tango
