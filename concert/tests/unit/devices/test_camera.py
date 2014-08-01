adbe84c9c7ee2c9e62d7e12177824aca53c19ac9
Tomas Farago <sensej007@email.cz>
2014-08-01 16:15:52 +0200

    Add PCO timestamp test

--- concert/tests/unit/devices/test_camera.py
+++ concert/tests/unit/devices/test_camera.py
@@ -1,7 +1,11 @@
+from datetime import datetime
+import numpy as np
+from nose.plugins.attrib import attr
 from concert.tests import TestCase
 from concert.coroutines.base import coroutine
 from concert.quantities import q
 from concert.devices.cameras.dummy import Camera, BufferedCamera
+from concert.devices.cameras.pco import Timestamp, TimestampError
 
 
 class TestDummyCamera(TestCase):
@@ -55,3 +59,30 @@ class TestDummyCamera(TestCase):
 
         self.camera.stream(check()).join()
         self.assertTrue(check.ok)
+
+
+@attr('skip-travis')
+class TestPCOTimeStamp(TestCase):
+    def test_valid(self):
+        image = np.empty((1, 14), dtype=np.uint16)
+        image[0] = np.array([0, 0, 0, 1, 32, 20, 8, 1, 20, 80, 69, 147, 40, 9], dtype=np.uint16)
+        stamp = Timestamp(image)
+        date_time = datetime(2014, 8, 1, 14, 50, 45, 932809)
+
+        self.assertEqual(stamp.time, date_time)
+        self.assertEqual(stamp.number, 1)
+
+    def test_bad_input(self):
+        image = np.empty((1,))
+        with self.assertRaises(TypeError):
+            Timestamp(image)
+
+        image = np.empty((1,), dtype=np.uint16)
+        with self.assertRaises(ValueError):
+            Timestamp(image)
+
+    def test_bad_data(self):
+        image = np.empty((1, 14), dtype=np.uint16)
+        image[0] = np.array([0, 0, 0, 1, 32, 20, 2**15, 1, 20, 80, 69, 147, 40, 9], dtype=np.uint16)
+        with self.assertRaises(TimestampError):
+            Timestamp(image)
