93d0a8cd8d00ca38c211639dbf8e72ba5d1b16aa
Tomas Farago <sensej007@email.cz>
2014-08-01 16:04:17 +0200

    Add Timestamp class for PCO cameras

--- concert/devices/cameras/pco.py
+++ concert/devices/cameras/pco.py
@@ -1,6 +1,8 @@
 """PCO cameras implementation."""
 
 import time
+from datetime import datetime
+import numpy as np
 from concert.devices.cameras import base
 from concert.devices.cameras.uca import Camera
 
@@ -88,3 +90,60 @@ class Dimax(Pco, base.BufferedMixin):
                 yield self.grab()
         finally:
             self.uca.stop_readout()
+
+
+class Timestamp(object):
+
+    """Read PCO's binary timestamp. *sequence* is an array of 14 unsigned short pixels."""
+
+    def __init__(self, image):
+        """Constructor."""
+        if image.dtype != np.uint16:
+            raise TypeError('Sequence must have type unsigned short int')
+        if len(image.shape) != 2 or image.shape[1] < 14:
+            raise ValueError('Image must be a 2D image and must be at least 14 pixels wide.')
+
+        # figures holds the individual figures from BCD-coded numbers, e.g a year composed of 4
+        # digits is stored in four separate entries
+        figures = []
+        for num in image[0, :14]:
+            # 16 bits per pixel, 4-bit BCD in the last 8 bits -> 2 decimal digits
+            bcd = np.binary_repr(num, width=16)[8:]
+            figures.append(int(bcd[:4], base=2))
+            figures.append(int(bcd[4:8], base=2))
+
+        try:
+            self._number = _concatenate_ints(figures[:8])
+
+            year = _concatenate_ints(figures[8:12])
+            month = _concatenate_ints(figures[12:14])
+            day = _concatenate_ints(figures[14:16])
+            hour = _concatenate_ints(figures[16:18])
+            minute = _concatenate_ints(figures[18:20])
+            sec = _concatenate_ints(figures[20:22])
+            usec = _concatenate_ints(figures[22:])
+            self._time = datetime(year, month, day, hour, minute, sec, usec)
+        except:
+            raise TimestampError('No valid timestamp found.')
+
+    @property
+    def number(self):
+        """Image number in the sequence."""
+        return self._number
+
+    @property
+    def time(self):
+        """Date and time when the image was taken."""
+        return self._time
+
+
+def _concatenate_ints(sequence):
+    """Construct a number from *sequence* of integers, e.g. [1, 2, 3] will be transformed to
+    123.
+    """
+    return int(''.join([str(num) for num in sequence]))
+
+
+class TimestampError(Exception):
+    """Bad timestamp error."""
+    pass
