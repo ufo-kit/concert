cb26b3e81a99cd4db93e9e869dd8609cf8f2a5b7
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2014-08-12 10:46:51 +0200

    Add DirectoryWalker unit test

--- concert/tests/unit/test_walker.py
+++ concert/tests/unit/test_walker.py
@@ -1,5 +1,9 @@
+import tempfile
+import shutil
+import numpy as np
+import os.path as op
 from concert.coroutines.base import coroutine, inject
-from concert.storage import Walker
+from concert.storage import Walker, DirectoryWalker, StorageError
 from concert.tests import TestCase
 
 
@@ -57,3 +61,33 @@ class TestWalker(TestCase):
     def test_generator(self):
         self.walker.write(data=self.data, fname='foo')
         self.check()
+
+
+class TestDirectoryWalker(TestCase):
+
+    def setUp(self):
+        super(TestDirectoryWalker, self).setUp()
+        self.path = tempfile.mkdtemp()
+        self.walker = DirectoryWalker(root=self.path)
+        self.data = np.ones((512, 512))
+
+    def tearDown(self):
+        shutil.rmtree(self.path)
+
+    def test_directory_creation(self):
+        self.walker.descend('foo')
+        self.walker.descend('bar')
+        self.assertTrue(op.exists(op.join(self.path, 'foo')))
+        self.assertTrue(op.exists(op.join(self.path, 'foo', 'bar')))
+
+    def test_default_write(self):
+        self.walker.write([self.data, self.data])
+        self.assertTrue(op.exists(op.join(self.path, 'frame_000000.tif')))
+        self.assertTrue(op.exists(op.join(self.path, 'frame_000001.tif')))
+
+        with self.assertRaises(StorageError):
+            self.walker.write([self.data])
+
+    def test_custom_write(self):
+        self.walker.write([self.data], fname='foo-{}.tif')
+        self.assertTrue(op.exists(op.join(self.path, 'foo-0.tif')))
