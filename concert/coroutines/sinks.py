b36d35c2d32b0a1fc8c0df3d292bad724aa766d8
Tomas Farago <tomas.farago@kit.edu>
2015-11-16 16:00:48 +0100

    Enable numpy-based Accumulate

--- concert/coroutines/sinks.py
+++ concert/coroutines/sinks.py
@@ -34,21 +34,41 @@ def null():
 
 
 class Accumulate(object):
-    """Accumulate items in a list."""
+    """Accumulate items in a list or a numpy array if *shape* is given, *dtype* is the data type.
+    """
 
-    def __init__(self):
-        self.items = []
+    def __init__(self, shape=None, dtype=None):
+        import numpy as np
 
-    @coroutine
+        self.items = [] if shape is None else np.empty(shape, dtype=dtype)
+
     def __call__(self):
         """
         __call__(self)
 
         Coroutine interface for processing in a pipeline.
         """
+        if isinstance(self.items, list):
+            return self._process()
+        else:
+            return self._process_numpy()
+
+    @coroutine
+    def _process(self):
+        """Stack data into a list."""
         # Clear results from possible previous execution
         self.items = []
 
         while True:
             item = yield
             self.items.append(item)
+
+    @coroutine
+    def _process_numpy(self):
+        """Stack data into a numpy array."""
+        i = 0
+
+        while True:
+            item = yield
+            self.items[i] = item
+            i += 1
