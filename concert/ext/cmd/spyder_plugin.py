2de79c9350cb16f78128aaf812b870045d0754c1
Tomas Farago <sensej007@email.cz>
2019-03-20 11:36:13 +0100

    spyder: update for spyder version 3 API
    fixes #424

--- concert/ext/cmd/spyder_plugin.py
+++ concert/ext/cmd/spyder_plugin.py
@@ -10,8 +10,15 @@ class SpyderCommand(Command):
         super(SpyderCommand, self).__init__('spyder', opts)
 
     def run(self, session):
-        from spyderlib import spyder
-        from spyderlib.config import CONF
+        try:
+            from spyderlib import spyder
+            from spyderlib.config import CONF
+            spyder_version = 2
+        except ImportError:
+            # Spyder 3
+            import spyder.app.mainwindow as spyder
+            from spyder.config.main import CONF
+            spyder_version = 3
 
         cs.exit_if_not_exists(session)
         app = spyder.initialize()
@@ -25,6 +32,9 @@ class SpyderCommand(Command):
                 'multithreaded': False,
                 'light': False,
                 'new_instance': True}
+        if spyder_version == 3:
+            opts['project'] = None
+            opts['window_title'] = 'Concert'
 
         # The python executable is set explicitly here, because Spyder tends
         # not to pick up the one used in a virtualenv. This should be set in a
