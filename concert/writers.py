a0662cc80628b9bb0bbe64af8d913622cf71a296
Tomas Farago <sensej007@email.cz>
2019-12-13 08:09:36 +0100

    writers: enable appending

--- concert/writers.py
+++ concert/writers.py
@@ -2,7 +2,7 @@
 
 
 class ImageWriter(object):
-    def __init__(self, filename, bytes_per_file):
+    def __init__(self, filename, bytes_per_file, append=False):
         self._writer = None
 
     def write(self, image):
@@ -13,18 +13,22 @@ class ImageWriter(object):
 
 
 class TiffWriter(ImageWriter):
-    def __init__(self, filename, bytes_per_file):
+    def __init__(self, filename, bytes_per_file, append=False):
         import tifffile
-        self._writer = tifffile.TiffWriter(filename, bigtiff=bytes_per_file >= 2 ** 31)
+        self._writer = tifffile.TiffWriter(filename, append=append,
+                                           bigtiff=bytes_per_file >= 2 ** 31)
 
     def write(self, image):
         self._writer.save(image)
 
 
 class LibTiffWriter(ImageWriter):
-    def __init__(self, filename, bytes_per_file):
+    def __init__(self, filename, bytes_per_file, append=False):
         from libtiff import TIFF
-        self._writer = TIFF.open(filename, 'w8' if bytes_per_file >= 2 ** 31 else 'w')
+        mode = 'a' if append else 'w'
+        if bytes_per_file >= 2 ** 31:
+            mode += '8'
+        self._writer = TIFF.open(filename, mode)
 
     def write(self, image):
         self._writer.write_image(image)
