12c040499ef4eb28cf8dfeeeb085aa8b40dcdda8
Tomas Farago <sensej007@email.cz>
2021-06-23 16:20:35 +0200

    Convert concert.progressbar to asyncio

--- concert/progressbar.py
+++ concert/progressbar.py
@@ -1,10 +1,12 @@
 """Progress bar allows you to see the progress of long-term operations."""
 
 import concert.config as cfg
+import inspect
 
 
 try:
     import tqdm
+    from tqdm.asyncio import tqdm as atqdm
 except:
     cfg.PROGRESS_BAR = False
 
@@ -12,6 +14,9 @@ except:
 def wrap_iterable(iterable, **kwargs):
     """Wrap *iterable* so that a progress bar will be shown on iteration."""
     if cfg.PROGRESS_BAR:
-        iterable = tqdm.tqdm(iterable, **kwargs)
+        if inspect.isasyncgen(iterable):
+            iterable = atqdm(iterable, **kwargs)
+        else:
+            iterable = tqdm.tqdm(iterable, **kwargs)
 
     return iterable
