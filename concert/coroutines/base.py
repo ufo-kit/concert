effc1044cbe20fdfdbbf821c2dd57171dba18ef2
Tomas Farago <sensej007@email.cz>
2021-07-23 17:13:41 +0200

    Improve coroutines.base

--- concert/coroutines/base.py
+++ concert/coroutines/base.py
@@ -1,20 +1,27 @@
 import asyncio
 import concert.config
-import functools
 import queue
 import logging
 import time
+from threading import Thread
 from concert.config import AIODEBUG
 from concert.helpers import PrioItem
 from concert.quantities import q
 
 
 LOG = logging.getLogger(__name__)
+# Loop which runs in a separate thread
+_TLOOP = None
+
+
+async def async_generate(iterable):
+    for item in iterable:
+        yield item
 
 
 def run_in_loop(coroutine):
-    """Wrap *coroutine* into a `asyncio.Task`, run it in the current loop and return the result.
-    On KeyboardInterrupt, the task is cancelled.
+    """Wrap *coroutine* into a `asyncio.Task`, run it in the current loop, block until it finishes
+    and return the result.  On KeyboardInterrupt, the task is cancelled.
     """
     loop = asyncio.get_event_loop()
     task = loop.create_task(coroutine)
@@ -25,6 +32,53 @@ def run_in_loop(coroutine):
     except KeyboardInterrupt:
         LOG.log(AIODEBUG, "KeyboardInterrupt in `%s', cancelling", task.get_coro().__qualname__)
         task.cancel()
+
+
+def run_in_loop_thread_blocking(coroutine):
+    """Run *coroutine* in a new loop in a separate thread. Useful for third party functions, which
+    are unaware of asyncio and we want to feed them with our asyncio code (like in the optimization
+    module).
+    """
+    global _TLOOP
+
+    def run():
+        _TLOOP.run_forever()
+
+    _TLOOP = asyncio.new_event_loop()
+    thread = Thread(target=run, daemon=False)
+    thread.start()
+
+    try:
+        future = asyncio.run_coroutine_threadsafe(coroutine, _TLOOP)
+        return future.result()
+    finally:
+        _TLOOP.call_soon_threadsafe(_TLOOP.stop)
+        thread.join()
+        _TLOOP.close()
+        _TLOOP = None
+
+
+def run_in_executor(func, *args):
+    """Run a blocking function *func* with signature func(*args) in an executor."""
+    # Leave this here for now, if there are problems with the normal run_in_executor returning a
+    # future then let's re-visit
+    # """Run a blocking function *func* with signature func(*args) in an executor, wrap the future
+    # into a real coroutine and return it.
+    # """
+    # async def make_coro():
+    #     loop = asyncio.get_running_loop()
+    #     future = loop.run_in_executor(None, func, *args)
+    #
+    #     try:
+    #         return await future
+    #     except asyncio.CancelledError:
+    #         # Wait for the future to finish even if we are cancelled.
+    #         LOG.log(AIODEBUG, f'{func.__name__} waiting for concurrent.Future')
+    #         return await future
+    # return make_coro()
+    loop = asyncio.get_event_loop()
+
+    return loop.run_in_executor(None, func, *args)
 
 
 def start(coroutine):
@@ -39,17 +93,13 @@ def start(coroutine):
     return asyncio.ensure_future(coroutine)
 
 
-# TODO: test
-def inloop(coroutine_func):
-    @functools.wraps(coroutine_func)
-    def inner(*args, **kwargs):
-        LOG.log(concert.config.AIODEBUG, 'inloop: %s', coroutine_func)
-        return run_in_loop(coroutine_func(*args, **kwargs))
-
-    return inner
+async def ensure_coroutine(func, *args, **kwargs):
+    """*func*(**args*, ***kwargs*) returns an awaitable which is wrapped here into a real coroutine.
+    This is useful for turuning futures from other libraries, like Tango, into real coroutines.
+    """
+    return await func(*args, **kwargs)
 
 
-# TODO: test
 def broadcast(producer, *consumers):
     """
     broadcast(producer, *consumers)
@@ -60,29 +110,56 @@ def broadcast(producer, *consumers):
     next_val = loop.create_future()
     consumed = loop.create_future()
     stop = object()
-    used = len(consumers)  # number of consumers
+    num_consumers = len(consumers)
+    used = num_consumers
 
     async def produce():
-        async for elem in producer:
-            next_val.set_result(elem)
-            await consumed
-        next_val.set_result(stop)
+        try:
+            async for elem in producer:
+                if not next_val.cancelled():
+                    next_val.set_result(elem)
+                await consumed
+                if not num_consumers:
+                    break
+            if not next_val.cancelled():
+                next_val.set_result(stop)
+        except Exception as e:
+            LOG.warn(f"Exception `{e}' in broadcast")
+            # Make sure the duplicates stop even if there is an exception
+            if not next_val.cancelled():
+                next_val.set_result(stop)
+            raise
 
     async def duplicate():
-        nonlocal next_val, consumed, used
-        while True:
-            val = await next_val
-            if val is stop:
-                return
-            yield val
+        nonlocal next_val, consumed, used, num_consumers
+        try:
+            while True:
+                val = await next_val
+                if val is stop:
+                    return
+                yield val
+                used -= 1
+                if not used:
+                    if not consumed.cancelled():
+                        consumed.set_result(None)
+                        consumed = loop.create_future()
+                        next_val = loop.create_future()
+                    used = num_consumers
+                else:
+                    await consumed
+        except Exception as e:
+            print('broadcast duplicate cancelled', e)
+            raise
+        finally:
+            # Our consumer called break in its async for, remove it from waiting list
+            num_consumers -= 1
             used -= 1
             if not used:
-                consumed.set_result(None)
-                consumed = loop.create_future()
-                next_val = loop.create_future()
-                used = len(consumers)
-            else:
-                await consumed
+                if not consumed.cancelled():
+                    consumed.set_result(None)
+                    consumed = loop.create_future()
+                    next_val = loop.create_future()
+                used = num_consumers
 
     started = [produce()]
     for consumer in consumers:
@@ -91,16 +168,15 @@ def broadcast(producer, *consumers):
     return started
 
 
-# TODO: test
 async def feed_queue(producer, func, *args):
     """Feed function *func* with items from *producer* in a separete thread. The signatute must be
     func(queue, *args) where elements in the queue are instances of `concert.helpers.PrioItem`.
     """
     loop = asyncio.get_running_loop()
     pqueue = queue.PriorityQueue()
-    future = loop.run_in_executor(None, func, pqueue, *args)
 
     try:
+        future = loop.run_in_executor(None, func, pqueue, *args)
         prio = 1
         async for item in producer:
             pqueue.put(PrioItem(priority=prio, data=item))
@@ -109,13 +185,13 @@ async def feed_queue(producer, func, *args):
         LOG.log(concert.config.AIODEBUG, f'feed_queue cancelled by exception {type(e)}')
         # Highest priority, effectively cancel processing of everything in the queue
         prio = 0
+        raise
     finally:
         LOG.log(concert.config.AIODEBUG, f'feed_queue finished with priority {prio}')
         pqueue.put(PrioItem(priority=prio, data=None))
         await future
 
 
-# TODO: test
 async def wait_until(condition, sleep_time=1e-1 * q.s, timeout=None):
     """Wait until a callable *condition* returns True. *sleep_time* is the time to sleep
     between consecutive checks of *condition*. If *timeout* is given and the *condition* doesn't
