aa6b03a53d8c803d935d6bb571427580932d1821
Tomas Farago <sensej007@email.cz>
2021-07-26 16:11:58 +0200

    Remove ENABLE_ASYNC
    from tests as well.

--- concert/config.py
+++ concert/config.py
@@ -1,9 +1,4 @@
 """
-.. data:: ENABLE_ASYNC
-
-    Enable casynchronous execution. If disabled, dummy futures are used that do
-    no execute synchronously.
-
 .. data:: MOTOR_VELOCITY_SAMPLING_TIME
 
     Time step for calculation of motor velocity by measuring two postion
@@ -16,9 +11,7 @@
 """
 from concert.quantities import q
 
-ENABLE_ASYNC = True
 # Prints the exception source by fake futures
-PRINT_NOASYNC_EXCEPTION = True
 PROGRESS_BAR = True
 
 MOTOR_VELOCITY_SAMPLING_TIME = 0.1 * q.s
--- runtests.py
+++ runtests.py
@@ -1,6 +1,5 @@
 import nose
-from concert.ext.noseplugin import DisableAsync
 import sys
 
 if __name__ == '__main__':
-    sys.exit(nose.main(addplugins=[DisableAsync()]))
+    sys.exit(nose.main())
