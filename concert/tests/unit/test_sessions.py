779056d9bad39c02944eca698598ab87b7ee66b4
Tomas Farago <sensej007@email.cz>
2021-09-07 11:12:40 +0200

    Test check_emergency_stop

--- concert/tests/unit/test_sessions.py
+++ concert/tests/unit/test_sessions.py
@@ -1,10 +1,10 @@
 """Test sessions."""
 import asyncio
-from unittest import TestCase
 from threading import Thread, Event
 from concert.coroutines.base import start, run_in_loop_thread_blocking
-from concert.session.utils import abort_awaiting
-from concert.tests import slow, suppress_logging
+from concert.quantities import q
+from concert.session.utils import abort_awaiting, check_emergency_stop
+from concert.tests import slow, suppress_logging, TestCase
 
 
 async def corofunc(inst):
@@ -21,6 +21,7 @@ class TestAborting(TestCase):
 
     def setUp(self):
         suppress_logging()
+        self.cancelled = False
         self.started = Event()
         super().setUp()
 
@@ -45,3 +46,37 @@ class TestAborting(TestCase):
         abort_awaiting(background=True)
         thread.join()
         self.assertEqual(result, None)
+
+    async def test_check_emergency_stop(self):
+        class Callable:
+            num_called = 0
+            aborted = False
+
+            def __call__(self):
+                result = False
+                if self.num_called == 2:
+                    self.aborted = True
+                    result = True
+
+                self.num_called += 1
+
+                return result
+
+        clb = Callable()
+        cancelled_task = start(corofunc(self))
+        task = start(check_emergency_stop(clb, poll_interval=1 * q.ms))
+
+        await asyncio.sleep(.1)
+
+        await cancelled_task
+        assert self.cancelled
+        assert clb.aborted
+        # This means callable has been called after the flag has been set, enabling it to wait for
+        # clearing the flag
+        assert clb.num_called > 3
+
+        # After the flag has been cleared, no more aborting of newly created tasks
+        running_task = start(corofunc(self))
+        await asyncio.sleep(.1)
+        await running_task
+        assert running_task.result() == 1
