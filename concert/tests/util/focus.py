b2b0fa63ef59972372d081937574e6122122a0cd
Tomas Farago <sensej007@email.cz>
2022-01-27 15:28:14 +0100

    tests: Proper Gaussian blur in BlurringCamera

--- concert/tests/util/focus.py
+++ concert/tests/util/focus.py
@@ -1,5 +1,5 @@
 import scipy.misc
-from scipy.ndimage import fourier
+from scipy.ndimage import gaussian_filter
 from concert.quantities import q
 from concert.devices.cameras.dummy import Base as DummyCameraBase
 
@@ -16,4 +16,4 @@ class BlurringCamera(DummyCameraBase):
 
     async def _grab_real(self):
         sigma = abs((await self.motor.get_position() - FOCUS_POSITION).magnitude)
-        return fourier.fourier_gaussian(self._original, sigma)
+        return gaussian_filter(self._original, sigma)
