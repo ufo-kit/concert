991a460b3c83376eeddf1ff29b667231662b70e3
Tomas Farago <sensej007@email.cz>
2022-02-23 10:08:41 +0100

    Add element selectors

---
+++ concert/devices/elementselectors/base.py
@@ -0,0 +1,30 @@
+"""
+ElementSelector module for setting discrete elements (filters, screens, intensity monitors, ...).
+Getter and Setter for element and the
+current state must be provided by the device implementation.
+"""
+
+from concert.base import State, AccessorNotImplementedError
+from concert.devices.base import Device
+
+
+class ElementSelector(Device):
+
+    """ElementSelector base class"""
+
+    state = State(default='standby')
+
+    def __init__(self):
+        super().__init__()
+
+    async def _set_element(self, element):
+        raise AccessorNotImplementedError
+
+    async def _get_element(self):
+        raise AccessorNotImplementedError
+
+
+class ElementSelectorError(Exception):
+    """ElementSelector related error"""
+
+    pass
