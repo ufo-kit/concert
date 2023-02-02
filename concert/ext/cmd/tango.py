22f9c2fc97acc99f8f97e1cc41bf0e2d0a3b29a6
Marcus Zuber <marcus.zuber@kit.edu>
2023-02-02 12:10:47 +0100

    make tango port settable and use meaningfully tango uris

--- concert/ext/cmd/tango.py
+++ concert/ext/cmd/tango.py
@@ -13,6 +13,11 @@ class TangoCommand(SubCommand):
                 'choices': SERVER_NAMES,
                 'help': 'Name of the tango server to run'
             },
+            '--port': {
+                'type': int,
+                'help': 'Port to run the server on',
+                'default': 1234
+            },
             '--logfile': {
                 'type': str
             },
@@ -24,24 +29,24 @@ class TangoCommand(SubCommand):
         }
         super(TangoCommand, self).__init__('tango', opts)
 
-    def run(self, server, logfile=None, loglevel=None):
+    def run(self, server, port, logfile=None, loglevel=None):
         import tango
         server_class = None
         if server == "benchmarker":
             from concert.ext.tangoservers import benchmarking
-            server_class = {'class': benchmarking.TangoBenchmarker, 'id': 1235}
+            server_class = {'class': benchmarking.TangoBenchmarker}
         if server == "dummycamera":
             from concert.ext.tangoservers import camera
-            server_class = {'class': camera.TangoDummyCamera, 'id': 1236}
+            server_class = {'class': camera.TangoDummyCamera}
         if server == "filecamera":
             from concert.ext.tangoservers import camera
-            server_class = {'class': camera.TangoFileCamera, 'id': 1236}
+            server_class = {'class': camera.TangoFileCamera}
         if server == "reco":
             from concert.ext.tangoservers import reco
-            server_class = {'class': reco.TangoOnlineReconstruction, 'id': 1237}
+            server_class = {'class': reco.TangoOnlineReconstruction}
         if server == "writer":
             from concert.ext.tangoservers import writer
-            server_class = {'class': writer.TangoWriter, 'id': 1238}
+            server_class = {'class': writer.TangoWriter}
 
         setup_logging(server, to_stream=True, filename=logfile, loglevel=loglevel)
 
@@ -49,11 +54,11 @@ class TangoCommand(SubCommand):
             args=[
                 'name',
                 '-ORBendPoint',
-                'giop:tcp::{}'.format(server_class['id']),
+                f'giop:tcp::{port}',
                 '-v4',
                 '-nodb',
                 '-dlist',
-                'a/b/c'
+                f'concert/tango/{server}'
             ],
             green_mode=tango.GreenMode.Asyncio
         )
