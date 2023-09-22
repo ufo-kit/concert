93875ecd360a5e28b975f51ffcebe85e774668cc
Chandan Sarkar <sarkar1986.chandan@gmail.com>
2023-09-22 18:56:24 +0200

    concert: Use running event loop

--- concert/persistence/handler.py
+++ concert/persistence/handler.py
@@ -47,7 +47,9 @@ class RemoteHandler(logging.Handler):
                 [f"{self._path}/{self._fname}", payload])
 
     def emit(self, record: logging.LogRecord) -> None:
-        asyncio.run(self.send(str(record)))
+        loop: asyncio.AbstractEventLoop = asyncio.get_running_loop()
+        asyncio.ensure_future(self.send(str(record)), loop=loop)
+
 
 if __name__ == "__main__":
     pass
