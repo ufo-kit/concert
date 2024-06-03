6e64458c3883789ad818e19a16228b9ac6faf56e
Tomas Farago <sensej007@email.cz>
2024-06-03 11:54:51 +0200

    SQUASH: dummy camera poisonpill

--- concert/devices/cameras/dummy.py
+++ concert/devices/cameras/dummy.py
@@ -269,13 +269,15 @@ class TangoCameraBase(base.RemoteMixin):
     async def _set_roi_height(self, value):
         await self._device.write_attribute('roi_height', value.magnitude)
 
-    async def _stop_sending(self):
+    async def stop_sending(self):
         await self._device.grab(0)
 
     async def _cancel_streaming(self):
         await self._device.cancel_grabbing()
 
-    async def _grab_send_real(self, num):
+    async def _grab_send_real(self, num, end=True):
+        # Tango does not support two arguments...
+        await self._device.write_attribute('send_poison_pill', end)
         await self._device.grab(num)
 
 
