07b9061373c6a53f34359a1274c6aa4a48ebaa6d
Tomas Farago <sensej007@email.cz>
2024-06-03 12:30:26 +0200

    SQUASH: cleanup synchrotron experiments

--- concert/experiments/synchrotron.py
+++ concert/experiments/synchrotron.py
@@ -2,13 +2,10 @@
 Module for synchrotron based imaging experiments.
 """
 from concert.quantities import q
-from concert.experiments.imaging import Radiography as BaseRadiography,\
-    SteppedTomography as BaseSteppedTomo, \
-    ContinuousTomography as BaseContTomo,\
-    ContinuousSpiralTomography as BaseContSpiralTomo, \
-    SteppedSpiralTomography as BaseSteppedSpiralTomo
+from concert.experiments import imaging
 
-from concert.experiments.imaging import GratingInterferometryStepping as BasePhaseStepping
+
+# Mixins
 
 
 class SynchrotronMixin:
@@ -17,8 +14,8 @@ class SynchrotronMixin:
     imaging experiments for the synchrotron by opening and closing a shutter.
     """
 
-    def __init__(self, shutter):
-        self._shutter = shutter
+    # The class implementing this must assign a real device here
+    _shutter = None
 
     async def start_sample_exposure(self):
         """
@@ -39,12 +36,17 @@ class SynchrotronMixin:
             await self._shutter.close()
 
 
-class Radiography(SynchrotronMixin, BaseRadiography):
+# Logic
+
+
+class RadiographyLogic(SynchrotronMixin, imaging.RadiographyLogic):
     """
-    Radiography experiment
+    Synchrotron radiography logic class which needs to be combined with one of the local or remote
+    mixins for DAQ.
     """
-    async def __ainit__(self, walker, flat_motor, radio_position, flat_position, camera, shutter,
-                        num_flats=200, num_darks=200, num_projections=3000, separate_scans=True):
+    async def __ainit__(self, walker, flat_motor, radio_position, flat_position, camera,
+                        shutter, num_flats=200, num_darks=200, num_projections=3000,
+                        separate_scans=True):
         """
         :param walker: Walker for storing experiment data.
         :type walker: concert.storage.Walker
@@ -56,7 +58,7 @@ class Radiography(SynchrotronMixin, BaseRadiography):
             beam. Unit must be the same as flat_motor['position'].
         :param camera: Camera to acquire the images.
         :type camera: concert.devices.cameras.base.Camera
-        :param shutter: Stutter
+        :param shutter: Shutter
         :type shutter: concert.devices.shutters.base.Shutter
         :param num_flats: Number of images for flatfield correction.
         :type num_flats: int
@@ -64,23 +66,27 @@ class Radiography(SynchrotronMixin, BaseRadiography):
         :type num_darks: int
         :param num_projections: Number of projections.
         :type num_projections: int
+        :param separate_scans: Descend into subdirectories based on current iteration.
+        :type separate_scans: bool
         """
-        SynchrotronMixin.__init__(self, shutter)
-        await BaseRadiography.__ainit__(
-            self, walker=walker,
-            flat_motor=flat_motor,
-            radio_position=radio_position,
-            flat_position=flat_position,
-            camera=camera, num_flats=num_flats,
-            num_darks=num_darks,
-            num_projections=num_projections,
+        self._shutter = shutter
+        await super().__ainit__(
+            walker,
+            flat_motor,
+            radio_position,
+            flat_position,
+            camera,
+            num_flats,
+            num_darks,
+            num_projections,
             separate_scans=separate_scans
         )
 
 
-class SteppedTomography(SynchrotronMixin, BaseSteppedTomo):
+class ContinuousTomographyLogic(SynchrotronMixin, imaging.ContinuousTomographyLogic):
     """
-    Stepped tomography
+    Continuous tomography logic class which needs to be combined with one of the local or remote
+    mixins for DAQ.
     """
     async def __ainit__(self, walker, flat_motor, tomography_motor, radio_position, flat_position,
                         camera, shutter, num_flats=200, num_darks=200, num_projections=3000,
@@ -90,15 +96,15 @@ class SteppedTomography(SynchrotronMixin, BaseSteppedTomo):
         :type walker: concert.storage.Walker
         :param flat_motor: Motor for moving sample in and out of the beam. Must feature a
             'position' property.
-        :param tomography_motor: RotationMotor for tomography scan.
+        :param tomography_motor: RotationMotor for tomography.
         :type tomography_motor: concert.devices.motors.base.RotationMotor
         :param radio_position: Position of *flat_motor* that the sample is positioned in the beam.
             Unit must be the same as flat_motor['position'].
         :param flat_position: Position of *flat_motor* that the sample is positioned out of the
             beam. Unit must be the same as flat_motor['position'].
         :param camera: Camera to acquire the images.
-        :type camera: concert.devices.camera.base.Camera
-        :param shutter: Stutter
+        :type camera: concert.devices.cameras.base.Camera
+        :param shutter: Shutter
         :type shutter: concert.devices.shutters.base.Shutter
         :param num_flats: Number of images for flatfield correction.
         :type num_flats: int
@@ -111,14 +117,14 @@ class SteppedTomography(SynchrotronMixin, BaseSteppedTomo):
         :param start_angle: Start position of *tomography_motor* for the first projection.
         :type start_angle: q.deg
         """
-        SynchrotronMixin.__init__(self, shutter)
-        await BaseSteppedTomo.__ainit__(
-            self, walker=walker,
-            flat_motor=flat_motor,
-            tomography_motor=tomography_motor,
-            radio_position=radio_position,
-            flat_position=flat_position,
-            camera=camera,
+        self._shutter = shutter
+        await super().__ainit__(
+            walker,
+            flat_motor,
+            tomography_motor,
+            radio_position,
+            flat_position,
+            camera,
             num_flats=num_flats,
             num_darks=num_darks,
             num_projections=num_projections,
@@ -128,9 +134,10 @@ class SteppedTomography(SynchrotronMixin, BaseSteppedTomo):
         )
 
 
-class ContinuousTomography(SynchrotronMixin, BaseContTomo):
+class SteppedTomographyLogic(SynchrotronMixin, imaging.SteppedTomographyLogic):
     """
-    Continuous tomography
+    Stepped tomography logic class which needs to be combined with one of the local or remote
+    mixins for DAQ.
     """
     async def __ainit__(self, walker, flat_motor, tomography_motor, radio_position, flat_position,
                         camera, shutter, num_flats=200, num_darks=200, num_projections=3000,
@@ -140,15 +147,15 @@ class ContinuousTomography(SynchrotronMixin, BaseContTomo):
         :type walker: concert.storage.Walker
         :param flat_motor: Motor for moving sample in and out of the beam. Must feature a
             'position' property.
-        :param tomography_motor: ContinuousRotationMotor for tomography scan.
-        :type tomography_motor: concert.devices.motors.base.ContinuousRotationMotor
+        :param tomography_motor: RotationMotor for tomography.
+        :type tomography_motor: concert.devices.motors.base.RotationMotor
         :param radio_position: Position of *flat_motor* that the sample is positioned in the beam.
             Unit must be the same as flat_motor['position'].
         :param flat_position: Position of *flat_motor* that the sample is positioned out of the
             beam. Unit must be the same as flat_motor['position'].
         :param camera: Camera to acquire the images.
-        :type camera: concert.devices.camera.base.Camera
-        :param shutter: Stutter
+        :type camera: concert.devices.cameras.base.Camera
+        :param shutter: Shutter
         :type shutter: concert.devices.shutters.base.Shutter
         :param num_flats: Number of images for flatfield correction.
         :type num_flats: int
@@ -161,14 +168,14 @@ class ContinuousTomography(SynchrotronMixin, BaseContTomo):
         :param start_angle: Start position of *tomography_motor* for the first projection.
         :type start_angle: q.deg
         """
-        SynchrotronMixin.__init__(self, shutter)
-        await BaseContTomo.__ainit__(
-            self, walker=walker,
-            flat_motor=flat_motor,
-            tomography_motor=tomography_motor,
-            radio_position=radio_position,
-            flat_position=flat_position,
-            camera=camera,
+        self._shutter = shutter
+        await super().__ainit__(
+            walker,
+            flat_motor,
+            tomography_motor,
+            radio_position,
+            flat_position,
+            camera,
             num_flats=num_flats,
             num_darks=num_darks,
             num_projections=num_projections,
@@ -178,9 +185,10 @@ class ContinuousTomography(SynchrotronMixin, BaseContTomo):
         )
 
 
-class ContinuousSpiralTomography(SynchrotronMixin, BaseContSpiralTomo):
+class ContinuousSpiralTomographyLogic(SynchrotronMixin, imaging.ContinuousSpiralTomographyLogic):
     """
-    Continuous spiral tomography
+    Continuous spiral tomography logic class which needs to be combined with one of the local or
+    remote mixins for DAQ.
     """
     async def __ainit__(self, walker, flat_motor, tomography_motor, vertical_motor, radio_position,
                         flat_position, camera, shutter, start_position_vertical, sample_height,
@@ -223,18 +231,18 @@ class ContinuousSpiralTomography(SynchrotronMixin, BaseContSpiralTomo):
         :param start_angle: Start position of *tomography_motor* for the first projection.
         :type start_angle: q.deg
         """
-        SynchrotronMixin.__init__(self, shutter)
-        await BaseContSpiralTomo.__ainit__(
-            self, walker=walker,
-            flat_motor=flat_motor,
-            tomography_motor=tomography_motor,
-            vertical_motor=vertical_motor,
-            radio_position=radio_position,
-            flat_position=flat_position,
-            camera=camera,
-            start_position_vertical=start_position_vertical,
-            sample_height=sample_height,
-            vertical_shift_per_tomogram=vertical_shift_per_tomogram,
+        self._shutter = shutter
+        await super().__ainit__(
+            walker,
+            flat_motor,
+            tomography_motor,
+            vertical_motor,
+            radio_position,
+            flat_position,
+            camera,
+            start_position_vertical,
+            sample_height,
+            vertical_shift_per_tomogram,
             num_flats=num_flats,
             num_darks=num_darks,
             num_projections=num_projections,
@@ -244,9 +252,10 @@ class ContinuousSpiralTomography(SynchrotronMixin, BaseContSpiralTomo):
         )
 
 
-class SteppedSpiralTomography(SynchrotronMixin, BaseSteppedSpiralTomo):
+class SteppedSpiralTomographyLogic(SynchrotronMixin, imaging.SteppedSpiralTomographyLogic):
     """
-    Stepped spiral tomography
+    Stepped spiral tomography logic class which needs to be combined with one of the local or
+    remote mixins for DAQ.
     """
     async def __ainit__(self, walker, flat_motor, tomography_motor, vertical_motor, radio_position,
                         flat_position, camera, shutter, start_position_vertical, sample_height,
@@ -288,18 +297,18 @@ class SteppedSpiralTomography(SynchrotronMixin, BaseSteppedSpiralTomo):
         :param start_angle: Start position of *tomography_motor* for the first projection.
         :type start_angle: q.deg
         """
-        SynchrotronMixin.__init__(self, shutter)
-        await BaseSteppedSpiralTomo.__ainit__(
-            self, walker=walker,
-            flat_motor=flat_motor,
-            tomography_motor=tomography_motor,
-            vertical_motor=vertical_motor,
-            radio_position=radio_position,
-            flat_position=flat_position,
-            camera=camera,
-            start_position_vertical=start_position_vertical,
-            sample_height=sample_height,
-            vertical_shift_per_tomogram=vertical_shift_per_tomogram,
+        self._shutter = shutter
+        await super().__ainit__(
+            walker,
+            flat_motor,
+            tomography_motor,
+            vertical_motor,
+            radio_position,
+            flat_position,
+            camera,
+            start_position_vertical,
+            sample_height,
+            vertical_shift_per_tomogram,
             num_flats=num_flats,
             num_darks=num_darks,
             num_projections=num_projections,
@@ -309,7 +318,89 @@ class SteppedSpiralTomography(SynchrotronMixin, BaseSteppedSpiralTomo):
         )
 
 
-class GratingInterferometryStepping(SynchrotronMixin, BasePhaseStepping):
+# Ready-to-use local and remote experiment implementations
+
+
+class LocalRadiography(imaging.LocalAutoDAQMixin, RadiographyLogic):
+    """
+    Radiography with local DAQ.
+    """
+    pass
+
+
+class RemoteRadiography(imaging.RemoteAutoDAQMixin, RadiographyLogic):
+    """
+    Radiography with remote DAQ.
+    """
+    pass
+
+
+class LocalSteppedTomography(imaging.LocalTriggerDAQMixin, SteppedTomographyLogic):
+    """
+    Stepped tomography with local DAQ.
+    """
+    pass
+
+
+class RemoteSteppedTomography(imaging.RemoteTriggerDAQMixin, SteppedTomographyLogic):
+    """
+    Stepped tomography with remote DAQ.
+    """
+    pass
+
+
+class LocalContinuousTomography(imaging.LocalAutoDAQMixin, ContinuousTomographyLogic):
+    """
+    Continuous tomography with local DAQ.
+    """
+    pass
+
+
+class RemoteContinuousTomography(imaging.RemoteAutoDAQMixin, ContinuousTomographyLogic):
+    """
+    Continuous tomography with remote DAQ.
+    """
+    pass
+
+
+class LocalSteppedSpiralTomography(imaging.LocalTriggerDAQMixin, SteppedSpiralTomographyLogic):
+    """
+    Stepped spiral tomography with local DAQ.
+    """
+    pass
+
+
+class RemoteSteppedSpiralTomography(imaging.RemoteTriggerDAQMixin, SteppedSpiralTomographyLogic):
+    """
+    Stepped spiral tomography with remote DAQ.
+    """
+    pass
+
+
+class LocalContinuousSpiralTomography(imaging.LocalAutoDAQMixin, ContinuousSpiralTomographyLogic):
+    """
+    Continuous spiral tomography with local DAQ.
+    """
+    pass
+
+
+class RemoteContinuousSpiralTomography(imaging.RemoteAutoDAQMixin, ContinuousSpiralTomographyLogic):
+    """
+    Continuous spiral tomography with remote DAQ.
+    """
+    pass
+
+
+class LocalGratingInterferometryStepping(
+    SynchrotronMixin,
+    imaging.LocalGratingInterferometryStepping
+):
+    """
+    Local synchrotron grating interferometry experiment.
+
+    Data can be automatically processed with the corresponding addon
+    (concert.experiments.addons.local.PhaseGratingSteppingFourierProcessing).
+    """
     async def __ainit__(self, walker, camera, shutter, flat_motor, stepping_motor, flat_position,
                         radio_position, grating_period, num_darks, stepping_start_position,
                         num_periods, num_steps_per_period, propagation_distance, separate_scans):
@@ -342,17 +433,17 @@ class GratingInterferometryStepping(SynchrotronMixin, BasePhaseStepping):
             by the processing addon to determine the phase shift in angles.
         :type propagation_distance: q.mm
         """
-        SynchrotronMixin.__init__(self, shutter)
-        await BasePhaseStepping.__ainit__(
-            self, walker=walker,
-            camera=camera,
-            flat_motor=flat_motor,
-            stepping_motor=stepping_motor,
-            flat_position=flat_position,
-            radio_position=radio_position,
-            grating_period=grating_period,
-            num_darks=num_darks,
-            stepping_start_position=stepping_start_position,
+        self._shutter = shutter
+        await super().__ainit__(
+            walker,
+            camera,
+            flat_motor,
+            stepping_motor,
+            flat_position,
+            radio_position,
+            grating_period,
+            num_darks,
+            stepping_start_position,
             num_periods=num_periods,
             num_steps_per_period=num_steps_per_period,
             propagation_distance=propagation_distance,
