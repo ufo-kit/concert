8782b4183d6323fca8faf77ab78689eb3e1f5e5a
Chandan Sarkar <sarkar1986.chandan@gmail.com>
2024-07-05 11:02:03 +0200

    feat: Include parameters

--- concert/ext/tangoservers/qa.py
+++ concert/ext/tangoservers/qa.py
@@ -1,8 +1,7 @@
 """
 qa.py
 -----
-Implements a device server to execute quality assurance routines during
-acquisition.
+Implements a device server to execute quality assurance routines during acquisition.
 """
 from typing import List, AsyncIterator
 import numpy as np
@@ -12,10 +11,10 @@ import scipy.optimize as sop
 import skimage.filters as skf
 import skimage.measure as sms
 from skimage.measure._regionprops import RegionProperties
-from tango import DebugIt, DevState, CmdArgType
+from tango import DebugIt, DevState, CmdArgType, EventType
 from tango.server import attribute, command, AttrWriteType
 from concert.ext.tangoservers.base import TangoRemoteProcessing
-from concert.ext.ufo import FlatCorrect, InjectProcess, get_task
+from concert.ext.ufo import FlatCorrect, MedianFilter 
 
 
 class QualityAssurance(TangoRemoteProcessing):
@@ -78,25 +77,46 @@ class QualityAssurance(TangoRemoteProcessing):
         doc="overall number of projections to be acquired"
     )
 
-    wait_interval = attribute(
-        label="Wait_Interval",
+    wait_window = attribute(
+        label="Wait_Window",
         dtype=int,
         access=AttrWriteType.READ_WRITE,
-        fget="get_wait_interval",
-        fset="set_wait_interval",
-        doc="initial interval to wait before starting rotation axis estimation"
+        fget="get_wait_window",
+        fset="set_wait_window",
+        doc="initial wait time before starting rotation axis estimation"
+    )
+
+    estm_offset = attribute(
+        label="Estm_Offset",
+        dtype=int,
+        access=AttrWriteType.READ_WRITE,
+        fget="get_estm_offset",
+        fset="set_estm_offset",
+        doc="offset in number of projections to be used to run estimation"
+    )
+
+    monitor_window = attribute(
+        label="Monitor_Window",
+        dtype=int,
+        access=AttrWriteType.READ_WRITE,
+        fget="get_monitor_window",
+        fset="set_monitor_window",
+        doc="monitoring window to detect plateau in rotation axis estimation slope"
     )
 
     _marker_centroids: List[ArrayLike]
     _point_in_time: int
     _angle_dist: ArrayLike
     _norm_buffer: List[ArrayLike]
+    _estm_rot_axis: List[float]
 
     async def init_device(self) -> None:
         await super().init_device()
+        EventType.EVE
         self._marker_centroids = []
         self._point_in_time = 0
         self._norm_buffer = []
+        self._estm_rot_axis = []
         self.info_stream("%s initialized device with state: %s",
                          self.__class__.__name__, self.get_state())
 
@@ -160,18 +180,38 @@ class QualityAssurance(TangoRemoteProcessing):
             self.__class__.__name__, self.get_num_radios(), self.get_state()
         )
 
-    def get_wait_interval(self) -> int:
-        return self._wait_interval
+    def get_wait_window(self) -> int:
+        return self._wait_window
 
-    def set_wait_interval(self, wait_interval: int) -> None:
-        self._wait_interval = wait_interval
+    def set_wait_window(self, wait_window: int) -> None:
+        self._wait_window = wait_window
         self.info_stream(
-            "%s has set wait onterval for estimation to: %d projections, state: %s",
-            self.__class__.__name__, self.get_wait_interval(), self.get_state()
+            "%s has set initial wait window for estimation to: %d projections, state: %s",
+            self.__class__.__name__, self.get_wait_window(), self.get_state()
+        )
+
+    def get_estm_offset(self) -> int:
+        return self._estm_offset
+
+    def set_estm_offset(self, estm_offset: int) -> None:
+        self._estm_offset = estm_offset
+        self.info_stream(
+            "%s has set estimation offset interval to: %d projections, state %s",
+            self.__class__.__name__, self.get_estm_offset(), self.get_state()
+        )
+
+    def get_monitor_window(self) -> int:
+        return self._monitor_window
+
+    def set_monitor_window(self, monitor_window: int) -> None:
+        self._monitor_window = monitor_window
+        self.info_stream(
+            "%s has set monitoring window to: %d projections, state %s",
+            self.__class__.__name__, self.get_monitor_window(), self.get_state()
         )
 
     @property
-    def _norm_interval(self) -> int:
+    def _norm_window(self) -> int:
         """
         Denotes total number of darks and flat fields combined. These images
         represent the ingredients for normalizing the projections. This is a
@@ -205,7 +245,7 @@ class QualityAssurance(TangoRemoteProcessing):
         async for image in producer:
             # Process normalization images
             self._point_in_time += 1
-            if self._point_in_time <= self._norm_interval:
+            if self._point_in_time <= self._norm_window:
                 self._norm_buffer.append(image)
                 self.info_stream("%s processed %d images for dark and flat fields",
                                  self.__class__.__name__, len(self._norm_buffer))
@@ -214,88 +254,46 @@ class QualityAssurance(TangoRemoteProcessing):
         dark: ArrayLike = np.array(self._norm_buffer[:self._num_darks]).mean(axis=0)
         flat: ArrayLike = np.array(self._norm_buffer[self._num_darks:self._num_flats]).mean(axis=0)
         ffc = FlatCorrect(dark=dark, flat=flat, absorptivity=False)
-        async for projection in ffc(producer):
+        mf = MedianFilter(kernel_size=self.get_lpf_size())
+        async for projection in mf(ffc(producer)):
             # Process projections
-            fc = snd.median_filter(projection, size=self.get_lpf_size())
-            thresh: ArrayLike = skf.threshold_multiotsu(fc, classes=3)
-            mask: ArrayLike = fc < thresh.min()
+            thresh: ArrayLike = skf.threshold_multiotsu(projection, classes=3)
+            mask: ArrayLike = projection < thresh.min()
             labels: ArrayLike = sms.label(mask)
             regions: List[RegionProperties] = sms.regionprops(label_image=labels)
             regions = sorted(regions, key=lambda region: region.area, reverse=True)
+            # TODO: Here we implicitly assume that the detected number of regions would always be
+            # greater than the expected number of markers. In practice this is an edge case because
+            # it might not always be true.
             if len(regions) > self.get_num_markers():
                 regions = regions[:self.get_num_markers()]
             self._marker_centroids.append(np.array(
                 list(map(lambda region: region.centroid, regions))))
-            try:
-                if self._point_in_time - self._norm_interval > self.get_wait_interval():
-                    rot_axes:  List[np.float64] = []
-                    # TODO: Think about the edge cases. Can this be guaranteed
-                    # that the segmentation algorithm would always be able to
-                    # detect desired number of markers and give us their centroids ?
-                    for mix in range(self.get_num_markers()):
-                        x: ArrayLike = self._angle_dist[:self._point_in_time - self._norm_interval]
-                        y: ArrayLike = np.array(self._marker_centroids)[:, mix, 1]
-                        params, _ = sop.curve_fit(f=self.opt_func,
-                                                  xdata=np.squeeze(x),
-                                                  ydata=np.squeeze(y))
-                        rot_axes.append(params[0])
-                    self.info_stream(
-                        "%s: Estimated rotation axis with projections [%d/%d]: %f",
-                        self.__class__.__name__,
-                        self._point_in_time - self._norm_interval,
-                        self._num_radios,
-                        np.median(rot_axes)
-                    )
-                else:
-                    self.info_stream(
-                        "%s: Skipping estimation until [%d/%d]",
-                        self.__class__.__name__,
-                        self._point_in_time - self._norm_interval,
-                        self._wait_interval
-                    )
-            except RuntimeError:
+            # We start estimating after initial wait window is passed. This is required for the
+            # optimization method to be able to yield some values for the parameters.
+            if self._point_in_time - self._norm_window < self._wait_window:
                 self.info_stream(
-                    "%s could not optimize parameters so far with projection: [%d/%d]",
+                    "%s: Skipping estimation until [%d/%d]",
                     self.__class__.__name__,
-                    self._point_in_time - self._norm_interval,
-                    self._num_radios
+                    self._point_in_time - self._norm_window,
+                    self._wait_window
                 )
-            self._point_in_time += 1
-
-    async def _compute_axis_numpy(self, producer: AsyncIterator[ArrayLike]) -> None:
-        async for image in producer:
-            self._point_in_time += 1
-            if self._point_in_time <= self._norm_interval:
-                self._norm_buffer.append(image)
-                self.info_stream("%s processed %d images for dark and flat fields",
-                                 self.__class__.__name__, len(self._norm_buffer))
             else:
-                dark: ArrayLike = np.array(self._norm_buffer[:self._num_darks]).mean(axis=0)
-                flat: ArrayLike = np.array(self._norm_buffer[self._num_darks:self._num_flats]).mean(axis=0)
-                fc: ArrayLike = np.nan_to_num(
-                    (image - dark)/(flat - dark),
-                    nan=0.0,
-                    posinf=0.0,
-                    neginf=0.0
-                )
-                fc = snd.median_filter(fc, size=self.get_lpf_size())
-                thresh: ArrayLike = skf.threshold_multiotsu(fc, classes=3)
-                mask: ArrayLike = fc < thresh.min()
-                labels: ArrayLike = sms.label(mask)
-                regions: List[RegionProperties] = sms.regionprops(label_image=labels)
-                regions = sorted(regions, key=lambda region: region.area, reverse=True)
-                if len(regions) > self.get_num_markers():
-                    regions = regions[:self.get_num_markers()]
-                self._marker_centroids.append(np.array(
-                    list(map(lambda region: region.centroid, regions))))
-                try:
-                    if self._point_in_time - self._norm_interval > self.get_wait_interval():
+                # We use an offset in terms of number of projections for estimation to gain speed
+                # up in the process.
+                if (self._point_in_time - self._norm_window) % self._estm_offset == 0:
+                    try:
                         rot_axes:  List[np.float64] = []
-                        # TODO: Think about the edge cases. Can this be guaranteed
-                        # that the segmentation algorithm would always be able to
-                        # detect desired number of markers and give us their centroids ?
+                        # TODO: This is a potential edge case. Can this be guaranteed that the
+                        # segmentation algorithm would always be able to detect desired number of
+                        # markers and give us their centroids ?
+                        # OR, a better idea could be that we infer the detected number of markers
+                        # from the shape of the accumulation array. That actually could introduce
+                        # errors in estimation. This needs to be explored and experimented. Ideal
+                        # solution is the simplest one that works reasonably well all the time and
+                        # not perfectly only some times.
                         for mix in range(self.get_num_markers()):
-                            x: ArrayLike = self._angle_dist[:self._point_in_time - self._norm_interval]
+                            x: ArrayLike = self._angle_dist[:self._point_in_time - self._norm_window]
                             y: ArrayLike = np.array(self._marker_centroids)[:, mix, 1]
                             params, _ = sop.curve_fit(f=self.opt_func,
                                                       xdata=np.squeeze(x),
@@ -304,24 +302,25 @@ class QualityAssurance(TangoRemoteProcessing):
                         self.info_stream(
                             "%s: Estimated rotation axis with projections [%d/%d]: %f",
                             self.__class__.__name__,
-                            self._point_in_time - self._norm_interval,
+                            self._point_in_time - self._norm_window,
                             self._num_radios,
                             np.median(rot_axes)
                         )
-                    else:
+                        self._estm_rot_axis.append(np.median(rot_axes))
+                        if (self._point_in_time - self._norm_window) % 100 == 0:
+                            self.info_stream(
+                                "Estimated gradient: %s",
+                                np.round(np.abs(np.gradient(self._estm_rot_axis)), decimals=4)
+                            )
+                    except RuntimeError:
                         self.info_stream(
-                            "%s: Skipping estimation until [%d/%d]",
+                            "%s could not find optimal parameters with projection: [%d/%d]",
                             self.__class__.__name__,
-                            self._point_in_time - self._norm_interval,
-                            self._wait_interval
+                            self._point_in_time - self._norm_window,
+                            self._num_radios
                         )
-                except RuntimeError:
-                    self.info_stream(
-                        "%s could not optimize parameters so far with projection: [%d/%d]",
-                        self.__class__.__name__,
-                        self._point_in_time - self._norm_interval,
-                        self._num_radios
-                    )
+            self._point_in_time += 1
+
 
 if __name__ == "__main__":
     pass
--- concert/ext/ufo.py
+++ concert/ext/ufo.py
@@ -1,10 +1,13 @@
 import asyncio
+from typing import AsyncIterator
 import copy
 import logging
+import sys
 import threading
 import time
-import sys
 import numpy as np
+from numpy.typing import ArrayLike
+
 
 try:
     import gi
@@ -231,6 +234,27 @@ class FlatCorrect(InjectProcess):
             yield await self.result(leave_index=0)
             first = False
 
+class MedianFilter(InjectProcess):
+    """Encapsulates UFO median filtering implementation"""
+
+    _mf: object
+
+    def __init__(self, kernel_size: int, copy_inputs: bool = False) -> None:
+        self._mf = get_task('median-filter')
+        self._mf.props.size = kernel_size
+        super().__init__(self._mf, get_output=True, output_dims=2, copy_inputs=copy_inputs)
+
+    async def __call__(self, producer: AsyncIterator[ArrayLike]) -> AsyncIterator[ArrayLike]:
+        """Co-routine compatible consumer."""
+        if not self._started:
+            self.start()
+        first = True
+        async for projection in producer:
+            if projection.dtype != np.float32:
+                projection: ArrayLike = projection.astype(np.float32)
+            await self.insert(projection, index=0)
+            yield await self.result(leave_index=0)
+            first = False
 
 class GeneralBackprojectArgs(object):
 
