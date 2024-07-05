8e51937009edcb1c308be73491fd5fe337eea032
Chandan Sarkar <sarkar1986.chandan@gmail.com>
2024-07-05 11:02:03 +0200

    feat: Include parameters

--- concert/qa/qa.py
+++ concert/qa/qa.py
@@ -11,7 +11,8 @@ class QualityAssurance(AsyncObject):
 
     async def __ainit__(self, device, num_darks: int, num_flats: int,
                         num_radios: int, num_markers: int, rot_angle: np.float64,
-                        wait_interval: int, lpf_size: int = 5) -> None:
+                        wait_window: int, lpf_size: int = 5, estm_offset: int = 5,
+                        monitor_window: int = 200) -> None:
         self._device = device
         await self._device.write_attribute("lpf_size", lpf_size)
         await self._device.write_attribute("num_markers", num_markers)
@@ -19,7 +20,9 @@ class QualityAssurance(AsyncObject):
         await self._device.write_attribute("num_darks", num_darks)
         await self._device.write_attribute("num_flats", num_flats)
         await self._device.write_attribute("num_radios", num_radios)
-        await self._device.write_attribute("wait_interval", wait_interval)
+        await self._device.write_attribute("wait_window", wait_window)
+        await self._device.write_attribute("estm_offset", estm_offset)
+        await self._device.write_attribute("monitor_window", monitor_window)
         await self._device.prepare_angular_distribution()
         await super().__ainit__()
 
