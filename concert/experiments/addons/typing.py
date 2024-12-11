9933d93f6f5b80357fbf7831bfcba5083b63d52f
Chandan Sarkar <sarkar1986.chandan@gmail.com>
2024-12-11 20:22:46 +0100

    feat: Fix interface method

--- concert/experiments/addons/typing.py
+++ concert/experiments/addons/typing.py
@@ -9,15 +9,15 @@ from typing import Protocol, Tuple
 class AbstractRAEDevice(Protocol):
 
     async def update_darks(self) -> None:
-        """Asynchronously accumulates the dark field projections and averages them"""
+        """Accumulates the dark field projections"""
         ...
 
     async def update_flats(self) -> None:
-        """Asynchronously accumulates the flat field projections and averages them"""
+        """Accumulates the flat field projections"""
         ...
 
-    async def estimate_center_of_rotation(self, args: Tuple[float]) -> None:
-        """Executes center of rotation estimation from radio projections"""
+    async def estimate_axis_of_rotation(self) -> None:
+        """Executes axis of rotation estimation from radio projections"""
         ...
 
 if __name__ == "__main__":
