41fd91ac2c2627ad127fac1b9ce313a330e6888e
Chandan Sarkar <sarkar1986.chandan@gmail.com>
2024-12-18 18:36:10 +0100

    feat: Revise docker orchestration

--- Dockerfile
+++ Dockerfile
@@ -1,35 +1,51 @@
 FROM ubuntu:22.04
 
+# Do not allow interactive terminal prompts
 ARG DEBIAN_FRONTEND=noninteractive
 
+# Set necessary environment variables
 ENV PIP_ROOT_USER_ACTION=ignore
 ENV G_MESSAGES_DEBUG=all
 ENV LD_LIBRARY_PATH=/usr/local/lib
+ENV PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+ENV GI_TYPELIB_PATH=/usr/local/lib/girepository-1.0
 
+# Install build dependencies
 RUN apt-get update && apt-get install -y \
     build-essential \
-    python3 \
-    python3-pip \
-    python3-setuptools \
-    python3-wheel \
     git \
     vim \
     cmake \
     libglib2.0-0 \
     libgtk+2.0-dev \
-    gobject-introspection \
+    libgirepository1.0-dev \
     ninja-build \
     libzmq5-dev \
-    libjson-glib-dev
+    libjson-glib-dev \
+    iputils-ping \
+    iproute2 \
+    netcat-traditional
 
+# Install Python 3.9 (Encountered issues with uca-net and Python 3.10)
+RUN apt-get install -y software-properties-common && \
+    add-apt-repository -y ppa:deadsnakes/ppa && \
+    apt-get update && \
+    apt install -y python3.9 \
+    python3-pip \
+    python3-setuptools \
+    python3-wheel
+
+# Install necessary Python packages
 RUN pip install --upgrade pip && pip install \
+    PyGObject==3.38.0 \
     numpy \
     pytango==9.5.0 \
     scikit-image \
-    meson
+    imageio
 
 WORKDIR /home
 
+# Install libuca and uca-net binaries
 RUN git clone https://github.com/ufo-kit/libuca.git && \
     # For uca-net repository it is temporarily required to checkout to uca_net_grab_send branch
     # until the same is merged to base branch.
@@ -51,10 +67,12 @@ RUN mkdir build && \
     make && \
     make install
 
+# Install concert from current branch
 WORKDIR /home/concert
 
 COPY . .
 
 RUN pip install -e .
 
-# Concert session location inside the container: /root/.local/share/concert/
+# NOTE: Concert session location inside the container: /root/.local/share/concert/. This
+# contatiner location would be mapped to test directory.
---
+++ concert/tests/scenarios/remote_experiment/session_remote_exp.py
@@ -0,0 +1,56 @@
+import asyncio
+import os
+import zmq
+import concert
+from concert.experiments.addons import tango as tango_addons
+from concert.quantities import q
+from concert.devices.motors.dummy import LinearMotor, ContinuousRotationMotor
+from concert.devices.shutters.dummy import Shutter
+from concert.storage import RemoteDirectoryWalker
+from concert.networking.base import get_tango_device
+from concert.experiments.synchrotron import RemoteContinuousTomography
+from concert.devices.cameras.uca import RemoteNetCamera
+from concert.helpers import CommData
+
+# Docker daemon creates a DNS entry inside the specified network with the service name. Therefore,
+# we need to specify that domain name to reach the walker on exposed port. In this case device_remote_walker
+# is the service, which was defined in the compose file and DNS entry would be creaed accordingly.
+walker_dev_uri = f"remote_walker:7001/concert/tango/walker#dbase=no"
+
+# Experimental configurations
+num_darks = 10
+num_flats = 10
+num_radios = 100
+
+# Communications Metadata
+SERVERS = {
+        "walker": CommData("uca_camera", port=8993, socket_type=zmq.PUSH),
+}
+
+# Camera
+camera = await RemoteNetCamera()
+if await camera.get_state() == 'recording':
+    await camera.stop_recording()
+
+# Walker | Writer Configuration
+root = "/mnt/image_mnt"
+walker_device = get_tango_device(walker_dev_uri, timeout=30 * 60 * q.s)
+walker = await RemoteDirectoryWalker(device=walker_device, root=root, bytes_per_file=2**40)
+
+# Experiment Configuration
+shutter = await Shutter()
+flat_motor = await LinearMotor()
+tomo = await ContinuousRotationMotor()
+exp = await RemoteContinuousTomography(walker=walker,
+                                       flat_motor=flat_motor, 
+                                       tomography_motor=tomo,
+                                       radio_position=0*q.mm,
+                                       flat_position=10*q.mm,
+                                       camera=camera,
+                                       shutter=shutter,
+                                       num_flats=num_flats,
+                                       num_darks=num_darks,
+                                       num_projections=num_radios)
+_ = await tango_addons.ImageWriter(exp, SERVERS["walker"], exp.acquisitions)
+
+_ = await exp.run()
