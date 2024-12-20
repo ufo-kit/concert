d5a3c1b763091191cc790722b4fba4f4886d0704
Chandan Sarkar <sarkar1986.chandan@gmail.com>
2024-12-20 17:21:44 +0100

    feat: Fix setup script

--- concert/tests/scenarios/remote_experiment/setup.py
+++ concert/tests/scenarios/remote_experiment/setup.py
@@ -6,6 +6,7 @@ from typing import List
 if __name__ == "__main__":
     base_path: Path = Path(os.environ["TARGET_LOCATION"])
     items: List[str] = os.listdir(base_path)
+    items = list(filter(lambda name: "scan" in name, items))
     if len(items) > 0:
         for item in items:
             abs_path: Path = base_path.joinpath(item)
