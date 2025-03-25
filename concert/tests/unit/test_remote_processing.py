e41d903e2816a21bb1f960ee05faafa96f01a5bd
Tomas Farago <sensej007@email.cz>
2025-03-25 17:42:11 +0100

    tests: remove tango camera

--- concert/tests/unit/test_remote_processing.py
+++ concert/tests/unit/test_remote_processing.py
@@ -98,12 +98,6 @@ class TestRemoteProcessingStartup(TestCase):
             f = await tango_dev.state()
             self.assertNotEqual(f, None)
 
-    async def test_camera_startup(self):
-        async with tango_run_concert('dummycamera', 20*q.s) as tango_uri:
-            tango_dev = get_tango_device(tango_uri)
-            f = await tango_dev.state()
-            self.assertNotEqual(f, None)
-
     async def test_reco_startup(self):
         if test_with_tofu:
             async with tango_run_concert('reco', 10*q.s) as tango_uri:
