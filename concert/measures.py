6f0e546e059f16fc67176db67c4be4849322fe3e
Chandan Sarkar <sarkar1986.chandan@gmail.com>
2025-08-13 17:37:47 +0200

    feat: Revise alignment param estimation

--- concert/measures.py
+++ concert/measures.py
@@ -3,8 +3,11 @@ Measures operate on some data and provide information about how \"good\"
 the data is.
 """
 import logging
+from typing import List, Tuple
 import numpy as np
-from concert.quantities import q
+import skimage.measure as skm
+from concert.quantities import q, Quantity
+from concert.typing import ArrayLike
 
 
 LOG = logging.getLogger(__name__)
@@ -223,3 +226,114 @@ def rotation_axis(tips):
     LOG.debug('Found z angle: %s, x angle: %s, center: %s', phi, psi, center)
 
     return (phi, psi, center)
+
+
+def estimate_alignment_parameters(centroids: List[ArrayLike],
+                                  min_data_pts: int = 5,
+                                  x_offset_px: int = 10,
+                                  use_svd: bool = False) -> Tuple[Quantity, Quantity, float]:
+    """
+    TODO: Include docstring.
+    """
+    
+    def normalized(angle: float) -> float:
+        """
+        Rotating the major axis by 180 degrees produces the same ellipse. For instance when actual
+        roll angle is approximately 0 degree, fitting noise can make the angle flip to approximately
+        180 degrees instead. We map the estimated angle to a smaller range of (-90°, 90°].
+        """
+        wrapped = (angle + np.pi) % (2 * np.pi) - np.pi
+        if wrapped > np.pi/2:
+            wrapped -= np.pi
+        elif wrapped <= -np.pi/2:
+            wrapped += np.pi
+        return wrapped
+
+    if len(centroids) < min_data_pts:
+        raise ValueError("at least 5 coordinate pairs are needed")
+    y_ind, x_ind = np.array(centroids)[:, 0], np.array(centroids)[:, 1]
+    if max(x_ind) - min(x_ind) < x_offset_px:
+        raise ValueError("sample off-centering too small, enlarge rotation radius.")
+    # We fit the projection centroids to the EllipseModel to determine if we have a proper ellipse.
+    elp = skm.EllipseModel()
+    is_ellipse = elp.estimate(data=np.array(centroids)[:, [1, 0]])
+    if is_ellipse:
+        xc, yc, semi_major, semi_minor, roll_angle_radians = elp.params
+    if is_ellipse and np.round(semi_minor).astype(int) >= 1:  # Proper Ellipse
+        if use_svd:
+            # Construct design matrix - system of equations from which we want to estimate the
+            # quadratic and linear parameters that best fit the ellipse. General equation of conic
+            # has six parameters, A B, C, D, E, F. Therefore design matrix has #data_points rows and
+            # six columns.
+            design_matrix = np.empty((len(x_ind), 6), dtype=float)
+            for i in range(len(x_ind)):
+                design_matrix[i] = np.array([x_ind[i] ** 2, x_ind[i] * y_ind[i],
+                                            y_ind[i] ** 2, x_ind[i], y_ind[i], 1])
+            # Center of a conic (Ellipse or Hyperbola) is the point where the gradient of the
+            # quadratic form becomes 0. To compute this (x, y) points we take the gradients of the
+            # general conic equation and make a linear system of equations out it. Upon solving the
+            # same using Cramer's rule we'd get the exact expressions for x_center and y_center
+            # below. Best-fit parameters come from the SVD of the design matrix above.
+            _, _, parmas_matrix = np.linalg.svd(design_matrix)
+            params = parmas_matrix.T[:, -1]
+            param_A, param_B, param_C = params[0], params[1], params[2]
+            param_D, param_E = params[3], params[4]
+            x_center = ((param_B * param_E - 2 * param_D * param_C) /
+                        (4 * param_A * param_C - param_B ** 2))
+            y_center = ((param_D * param_B - 2 * param_A * param_E) /
+                        (4 * param_A * param_C - param_B ** 2))
+            center = np.array([y_center, x_center])
+            # We take two points out of the sphere centroids which make the projected ellipse,
+            # remove the pre-existing translations in physical space and get two direction vectors
+            # from origin. These would be used to determine the sign (rotation direction) of the
+            # pitch angle. We can compute the magnitude of the pitch from singular values but to get
+            # the directional information we use dot product, cross product with inverse tangent.
+            # Dot product provides the magnitude of, how two direction vectors are aligned with each
+            # other and brings in the cosine component.
+            # Cross product for the 2D direction vectors provide the area of the paralleogram that
+            # they make and depending on the orientation of the vectors with respect to each other
+            # it can be positive or negative. Here it is supposed to give us the hint, whether
+            # dir_vec_1 is to the clockwise or anticlockwise direction from dir_vec_0 and it brings
+            # in the sine component.
+            dir_vec_0 = np.array(centroids[0]) - center
+            dir_vec_1 = np.array(centroids[1]) - center
+            pitch_d_angle = np.arctan2(np.cross(dir_vec_0, dir_vec_1), np.dot(dir_vec_0, dir_vec_1))
+            sign = int(np.sign(pitch_d_angle))
+            quadratic_form_matrix = np.array([[param_A, param_B / 2], [param_B / 2, param_C]])
+            # Quadratic form matrix is symmetric. Therefore its singular values and singular vectors
+            # are identical to eigen values and eigen vectors. But for consistency we name them
+            # sing_vals and sign_vecs. Following SVD in principle has,
+            # # first matrix = eigen vectors along semi-major and semi-minor axes as column vectors,
+            # # second diagonal matrix = corresponding eigen values,
+            # # third matrix = first matrix transposed i.e. same eigen vectors as row vectors.
+            _, sing_vals, _ = np.linalg.svd(quadratic_form_matrix)
+            # The ratio of sqrt(eigen values) along semi-minor and semi-major axes is proportional
+            # to the ratio of semi-minor and semi-major axes. Normally eigen values and eigen
+            # vectors are in decreasing order, hence sing_vals[0] is the eigen value along major
+            # axis and sing_vals[1] is he eigen value along minor axis. In projected ellipse their
+            # invserse sine gives us the magnitude of how much we are tilted. On top of that we
+            # incorporate the direction.
+            pitch_ang = sign * np.arcsin(np.sqrt(sing_vals[1]) / np.sqrt(sing_vals[0])) * q.rad
+        else:
+            # Instead of using SVD we use the optimal parameters from the least square ellipse fit.
+            center = np.array([yc, xc])
+            dir_vec_0 = np.array(centroids[0]) - center
+            dir_vec_1 = np.array(centroids[1]) - center
+            pitch_d_angle = np.arctan2(np.cross(dir_vec_0, dir_vec_1), np.dot(dir_vec_0, dir_vec_1))
+            sign = int(np.sign(pitch_d_angle))
+            pitch_ang = sign * np.arcsin(semi_minor / semi_major) * q.rad
+        roll_ang = normalized(roll_angle_radians) * q.rad
+    else:  # Degenerate Ellipse
+        # In this scenario we make use of the slope (rise / run) and inverse tanget to derive the
+        # roll angle.
+        d_y = float(y_ind.max() - y_ind.min())
+        d_x = float(x_ind.max() - x_ind.min())
+        angle = normalized(np.arctan2(d_y , d_x)) * q.rad
+        # We are determining the correct sign for the roll angle under the assumption that our
+        # coordinate origin is at the bottom-left. It is because we assume y-axis to the vertical
+        # direction.
+        if x_ind[y_ind.argmax()] <= x_ind[y_ind.argmin()]:
+            angle = -angle
+        roll_ang, pitch_ang = angle, 0.0 * q.rad
+        center = np.mean(x_ind)
+    return roll_ang.to(q.deg), pitch_ang.to(q.deg), center
