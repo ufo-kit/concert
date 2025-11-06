895c93d23440af5d117bd48ec405509e2794ce89
Marcus Zuber <marcus.zuber@kit.edu>
2025-11-06 09:45:52 +0100

    Make TangoAddon._endpoint and TangoAddon.endpoint consistent

--- concert/experiments/addons/tango.py
+++ concert/experiments/addons/tango.py
@@ -125,7 +125,7 @@ class TangoMixin:
     async def __ainit__(self, device, endpoints: dict | CommData) -> Awaitable:
         self._device = device
         self._device.set_timeout_millis(DISTRIBUTED_TANGO_TIMEOUT)
-        self._endpoints = endpoints
+        self.endpoints = endpoints
 
     async def _teardown(self):
         await self._device.disconnect_endpoint()
@@ -140,11 +140,11 @@ class Benchmarker(TangoMixin, base.Benchmarker):
     def _make_consumers(self, acquisitions):
         consumers = {}
 
-        use_same_endpoint = type(self._endpoints) is CommData
+        use_same_endpoint = type(self.endpoints) is CommData
 
         for acq in acquisitions:
             consumers[acq] = TangoConsumer(
-                self._device, self._endpoints if use_same_endpoint else self._endpoints[acq.name], self.start_timer, corofunc_args=(acq.name,)
+                self._device, self.endpoints if use_same_endpoint else self.endpoints[acq.name], self.start_timer, corofunc_args=(acq.name,)
             )
 
         return consumers
@@ -188,11 +188,11 @@ class ImageWriter(TangoMixin, base.ImageWriter):
 
             return write_sequence
 
-        use_same_endpoint = type(self._endpoints) is CommData
+        use_same_endpoint = type(self.endpoints) is CommData
 
         for acq in acquisitions:
             consumers[acq] = TangoConsumer(
-                self._device, self._endpoints if use_same_endpoint else self._endpoints[acq.name], prepare_wrapper(acq.name)
+                self._device, self.endpoints if use_same_endpoint else self.endpoints[acq.name], prepare_wrapper(acq.name)
             )
 
         return consumers
@@ -213,10 +213,10 @@ class LiveView(base.LiveView):
         async def consume():
             pass
 
-        use_same_endpoint = type(self._endpoints) is CommData
+        use_same_endpoint = type(self.endpoints) is CommData
 
         for acq in acquisitions:
-            consumers[acq] = LiveViewConsumer(self._viewer, self._endpoints if use_same_endpoint else  self.endpoints[acq.name], consume)
+            consumers[acq] = LiveViewConsumer(self._viewer, self.endpoints if use_same_endpoint else  self.endpoints[acq.name], consume)
 
         return consumers
 
@@ -274,19 +274,19 @@ class OnlineReconstruction(TangoMixin, base.OnlineReconstruction):
         flats = base.get_acq_by_name(acquisitions, 'flats')
         radios = base.get_acq_by_name(acquisitions, 'radios')
 
-        use_same_endpoint = type(self._endpoints) is CommData
+        use_same_endpoint = type(self.endpoints) is CommData
 
 
         if self._do_normalization:
             consumers[darks] = TangoConsumer(
-                self._device, self._endpoints if use_same_endpoint else self._endpoints[darks.name], self.update_darks
+                self._device, self.endpoints if use_same_endpoint else self.endpoints[darks.name], self.update_darks
             )
             consumers[flats] = TangoConsumer(
-                self._device, self._endpoints if use_same_endpoint else self._endpoints[flats.name], self.update_flats
+                self._device, self.endpoints if use_same_endpoint else self.endpoints[flats.name], self.update_flats
             )
 
         consumers[radios] = TangoConsumer(
-            self._device, self._endpoints if use_same_endpoint else self._endpoints[radios.name], self.reconstruct
+            self._device, self.endpoints if use_same_endpoint else self.endpoints[radios.name], self.reconstruct
         )
 
         return consumers
